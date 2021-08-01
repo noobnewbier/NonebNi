d0be49150578ad411f02ee66a6669964597589a8
noobnewbier <peter789b@gmail.com>
2021-08-01 17:34:45 +0900

    Added PlayModeStateListener.cs for editor implementation

---
+++ Assets/Code/NonebNi/Editor/Maps/PlayModeStateListener.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEditor;
+
+namespace NonebNi.Editor.Maps
+{
+    /// <summary>
+    /// Stripped from UnityEditor.ProGrids.PlayModeStateListener
+    /// </summary>
+    [InitializeOnLoad]
+    internal class PlayModeStateListener
+    {
+#pragma warning disable 649
+        // ReSharper disable MemberCanBePrivate.Global
+        internal static event Action? OnEnterPlayMode;
+        internal static event Action? OnExitPlayMode;
+        internal static event Action? OnEnterEditMode;
+        internal static event Action? OnExitEditMode;
+        // ReSharper restore MemberCanBePrivate.Global
+#pragma warning restore 649
+
+        static PlayModeStateListener()
+        {
+            EditorApplication.playModeStateChanged += (x) =>
+            {
+                switch (x)
+                {
+                    case PlayModeStateChange.EnteredEditMode:
+                        OnEnterEditMode?.Invoke();
+                        break;
+                    case PlayModeStateChange.ExitingEditMode:
+                        OnExitEditMode?.Invoke();
+                        break;
+                    case PlayModeStateChange.EnteredPlayMode:
+                        OnEnterPlayMode?.Invoke();
+                        break;
+                    case PlayModeStateChange.ExitingPlayMode:
+                        OnExitPlayMode?.Invoke();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(x), x, null);
+                }
+            };
+        }
+    }
+}
