ef8a7fb56c9c90eb868e3a39efc7af75fc4a0dc7
noobnewbier <peter789b@gmail.com>
2021-08-08 17:58:01 +0900

    added level data

---
+++ Assets/Code/NonebNi/Core/Level/LevelData.cs
@@ -0,0 +1,18 @@
+﻿using NonebNi.Core.Maps;
+
+namespace NonebNi.Core.Level
+{
+    public class LevelData
+    {
+        public MapConfig MapConfig { get; }
+        public WorldConfig WorldConfig { get; }
+        public Map Map { get; }
+
+        public LevelData(MapConfig mapConfig, WorldConfig worldConfig, Map map)
+        {
+            MapConfig = mapConfig;
+            WorldConfig = worldConfig;
+            Map = map;
+        }
+    }
+}
---
+++ Assets/Code/NonebNi/Core/Level/LevelDataScriptable.cs
@@ -0,0 +1,16 @@
+﻿using NonebNi.Core.Maps;
+using UnityEngine;
+using UnityUtils.Constants;
+
+namespace NonebNi.Core.Level
+{
+    /// <summary>
+    /// Created this as we anticipate the need for custom BGMs and stuffs in the future
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(LevelDataScriptable), menuName = MenuName.Data + nameof(LevelDataScriptable))]
+    public class LevelDataScriptable : ScriptableObject
+    {
+        public MapConfig mapConfig;
+        public WorldConfig worldConfig;
+    }
+}
