a0e55b8a76e7538ac94bb72aca97c46af3c83d42
noobnewbier <peter789b@gmail.com>
2021-08-09 18:18:31 +0900

    stop drawing label when we are too far away from the tile

--- Assets/Code/NonebNi/Editor/Maps/Grid/GridView.cs
+++ Assets/Code/NonebNi/Editor/Maps/Grid/GridView.cs
@@ -45,7 +45,7 @@ namespace NonebNi.Editor.Maps.Grid
 
                 var center = GetTilePosition(_worldConfig.MapStartingPosition.y, tile.Coordinate, _worldConfig);
                 // Handles.Label(center, $"{tile.Coordinate}", _coordinateStyle);
-                DrawCenteredLabel(center, $"{tile.Coordinate}");
+                DrawCenteredLabel(center, $"{tile.Coordinate}", _worldConfig.InnerRadius);
                 var corners = _worldConfig.TileCornersOffset.Select(c => center + c).ToList();
                 Handles.DrawLine(corners[0], corners[5]);
                 for (var i = 0; i < corners.Count - 1; i++) Handles.DrawLine(corners[i], corners[i + 1]);
@@ -53,26 +53,25 @@ namespace NonebNi.Editor.Maps.Grid
         }
 
         /// <summary>
-        /// <see cref="Handles.Label(UnityEngine.Vector3,string)" /> doesn't seems to be working with centered text properly.
-        /// My speculation is that <see cref="Handles.Label(UnityEngine.Vector3,string)" /> is creating a temporary <see cref="GUIContent" /> through a method
-        /// <see cref="EditorGUIUtility.TempContent(string)" /> and for some reason this leads calculating the rect width correctly,
-        /// and hence the text box is not offset correctly.
-        /// This implementation is basically copying the internals of <see cref="Handles.Label(UnityEngine.Vector3,string)" />, except we didn't use the
-        /// temporary GUI Content
-        /// Any how using this should do the trick.
+        /// <see cref="Handles.Label(UnityEngine.Vector3,string)" /> can't center the label properly.
+        /// Root of the problem is that <see cref="HandleUtility.WorldPointToSizedRect" /> isn't taking rects maxX/Y into account,
+        /// This leads to a problem where the rect is essentially stretched to the left a bit, and as a result when trying to center the item,
+        /// the text will goes a bit "righter" than it should be.
+        /// Until Unity(current ver: 2020.2.7f1) fix this, this should do the trick
         /// </summary>
         /// <param name="position"></param>
         /// <param name="text"></param>
+        /// <param name="maxOffsetFromCenter"></param>
         /// <returns></returns>
-        private void DrawCenteredLabel(Vector3 position, string text)
+        private void DrawCenteredLabel(Vector3 position, string text, float maxOffsetFromCenter)
         {
             //behind the camera
             if (HandleUtility.WorldToGUIPointWithDepth(position).z < 0.0)
                 return;
 
-            Handles.BeginGUI();
-            GUIStyle coordinateStyle = new GUIStyle {alignment = TextAnchor.MiddleCenter};
+
             var guiPoint = HandleUtility.WorldToGUIPoint(position);
+            var coordinateStyle = new GUIStyle {alignment = TextAnchor.MiddleCenter};
             var textContent = EditorGUIUtility.TrTempContent(text);
             var size = coordinateStyle.CalcSize(textContent);
             var rect = new Rect(guiPoint, size);
@@ -80,7 +79,21 @@ namespace NonebNi.Editor.Maps.Grid
             rect.xMax -= size.x / 2;
             rect.yMin -= size.y / 2;
             rect.yMax -= size.y / 2;
+
+            //stop drawing the label when the text is so large, it goes "out of the bounds"(in our use case, the tile itself)
+            //A text goes out of bounds when the min/max point distance to center is greater than the maximum offset
+            var minAcceptableX = HandleUtility.WorldToGUIPoint(position + Vector3.left * maxOffsetFromCenter).x;
+            var maxAcceptableX = HandleUtility.WorldToGUIPoint(position + Vector3.right * maxOffsetFromCenter).x;
+            var minAcceptableY = HandleUtility.WorldToGUIPoint(position + Vector3.back * maxOffsetFromCenter).y;
+            var maxAcceptableY = HandleUtility.WorldToGUIPoint(position + Vector3.forward * maxOffsetFromCenter).y;
+            if (!(minAcceptableX < rect.xMin || maxAcceptableX > rect.xMax || minAcceptableY < rect.yMin || maxAcceptableY > rect.yMax))
+                //todo: better handle cases where the camera is far from the label
+                return;
+
+            Handles.BeginGUI();
+
             GUI.Label(coordinateStyle.padding.Add(rect), textContent, coordinateStyle);
+
             Handles.EndGUI();
         }
 
