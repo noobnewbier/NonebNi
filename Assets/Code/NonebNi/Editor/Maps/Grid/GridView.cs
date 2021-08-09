79a0a5133dffb6827f569589c66c716859ed276c
noobnewbier <peter789b@gmail.com>
2021-08-09 17:41:42 +0900

    adding coordinate into grid view

--- Assets/Code/NonebNi/Editor/Maps/Grid/GridView.cs
+++ Assets/Code/NonebNi/Editor/Maps/Grid/GridView.cs
@@ -39,13 +39,49 @@ namespace NonebNi.Editor.Maps.Grid
             if (_worldConfig == null) return;
 
             var grid = _map.GetGridForType<Tile>();
+            foreach (var tile in grid)
+            {
+                if (tile == null) continue;
 
-            var positions = from Tile? tile in grid
-                where tile != null
-                select GetTilePosition(_worldConfig.MapStartingPosition.y, new Coordinate(tile.Coordinate.X, tile.Coordinate.Z), _worldConfig);
+                var center = GetTilePosition(_worldConfig.MapStartingPosition.y, tile.Coordinate, _worldConfig);
+                // Handles.Label(center, $"{tile.Coordinate}", _coordinateStyle);
+                DrawCenteredLabel(center, $"{tile.Coordinate}");
+                var corners = _worldConfig.TileCornersOffset.Select(c => center + c).ToList();
+                Handles.DrawLine(corners[0], corners[5]);
+                for (var i = 0; i < corners.Count - 1; i++) Handles.DrawLine(corners[i], corners[i + 1]);
+            }
+        }
 
-            var vertices = (from pos in positions from corner in _worldConfig.TileCornersOffset select pos + corner).ToList();
-            for (var i = 1; i < vertices.Count; i++) Handles.DrawLine(vertices[i], i % 6 == 5 ? vertices[i - 5] : vertices[i + 1]);
+        /// <summary>
+        /// <see cref="Handles.Label(UnityEngine.Vector3,string)" /> doesn't seems to be working with centered text properly.
+        /// My speculation is that <see cref="Handles.Label(UnityEngine.Vector3,string)" /> is creating a temporary <see cref="GUIContent" /> through a method
+        /// <see cref="EditorGUIUtility.TempContent(string)" /> and for some reason this leads calculating the rect width correctly,
+        /// and hence the text box is not offset correctly.
+        /// This implementation is basically copying the internals of <see cref="Handles.Label(UnityEngine.Vector3,string)" />, except we didn't use the
+        /// temporary GUI Content
+        /// Any how using this should do the trick.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private void DrawCenteredLabel(Vector3 position, string text)
+        {
+            //behind the camera
+            if (HandleUtility.WorldToGUIPointWithDepth(position).z < 0.0)
+                return;
+
+            Handles.BeginGUI();
+            GUIStyle coordinateStyle = new GUIStyle {alignment = TextAnchor.MiddleCenter};
+            var guiPoint = HandleUtility.WorldToGUIPoint(position);
+            var textContent = EditorGUIUtility.TrTempContent(text);
+            var size = coordinateStyle.CalcSize(textContent);
+            var rect = new Rect(guiPoint, size);
+            rect.xMin -= size.x / 2;
+            rect.xMax -= size.x / 2;
+            rect.yMin -= size.y / 2;
+            rect.yMax -= size.y / 2;
+            GUI.Label(coordinateStyle.padding.Add(rect), textContent, coordinateStyle);
+            Handles.EndGUI();
         }
 
         private Vector3 GetTilePosition(float yPosition, Coordinate coordinate, WorldConfigData worldConfig)
