c42298662b1993d0268ea2bd236490ee43459fd9
noobnewbier <peter789b@gmail.com>
2021-10-31 05:23:44 +0900

    Stripping editor specific components on build

---
+++ Assets/Code/NonebNi/Editors/BuildPostProcessor/HelperComponentStripper.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NonebNi.Editors.Level.Entities;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NonebNi.Editors.BuildPostProcessor
+{
+    public class HelperComponentStripper : IProcessSceneWithReport
+    {
+        public int callbackOrder { get; }
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            var roots = scene.GetRootGameObjects();
+
+            var entities = roots.SelectMany(r => r.GetComponentsInChildren<Entity>());
+            foreach (var entity in entities) Object.DestroyImmediate(entity);
+        }
+    }
+}
