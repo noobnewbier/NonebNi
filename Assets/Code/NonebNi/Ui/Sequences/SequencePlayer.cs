41be7cc5007a532335cdf4ae98a4bdaf7a8143f5
noobnewbier <peter789b@gmail.com>
2022-01-19 23:23:16 +0900

    Added sequence player to play any animation from a given ISequence

---
+++ Assets/Code/NonebNi/Ui/Sequences/SequencePlayer.cs
@@ -0,0 +1,27 @@
+﻿using NonebNi.Core.Sequences;
+using NonebNi.Ui.Entities;
+using UnityEngine;
+
+namespace NonebNi.Ui.Sequences
+{
+    public class SequencePlayer : ISequencePlayer
+    {
+        private readonly IEntityRepository _entityRepository;
+
+        public SequencePlayer(IEntityRepository entityRepository)
+        {
+            _entityRepository = entityRepository;
+        }
+
+        public Coroutine? Play(ISequence sequence)
+        {
+            if (sequence is DieSequence dieSequence)
+            {
+                var entity = _entityRepository.GetEntity(dieSequence.DeadUnit.Guid);
+                if (entity != null) return entity.GetAnimationControl<PrototypeDieAnimationControl>().Play(new Context());
+            }
+
+            return null;
+        }
+    }
+}
