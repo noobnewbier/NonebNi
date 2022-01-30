b43672287540edb2c943394479e2ab0a2e4d9e04
noobnewbier <peter789b@gmail.com>
2022-01-31 01:09:11 +0900

    feat(debug-console): adding expression parser

---
+++ Assets/Code/NonebNi/EditorConsole/Parsers/ExpressionParser.DamageConsoleCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NonebNi.EditorConsole.Commands;
+using NonebNi.EditorConsole.Expressions;
+
+namespace NonebNi.EditorConsole.Parsers
+{
+    public partial class ExpressionParser
+    {
+        private static IConsoleCommand? ParseDamageConsoleCommand(IReadOnlyList<Expression> expressions)
+        {
+            if (!(expressions.First() is CommandName { Value: "damage" })) return null;
+
+            if (expressions.Count != 3) return new ErrorMessageConsoleCommand(@"invalid format for ""damage""");
+
+            if (!(expressions[1] is CoordinateParameter coordinateParameter))
+                return new ErrorMessageConsoleCommand(@"invalid format for ""damage""");
+
+            if (!(expressions[2] is IntParameter damageParam))
+                return new ErrorMessageConsoleCommand(@"invalid format for ""damage""");
+
+            return new DamageConsoleCommand(coordinateParameter.Value, damageParam.Value);
+        }
+    }
+}
