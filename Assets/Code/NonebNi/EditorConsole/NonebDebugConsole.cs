7c6542315642aca32fdf17c67a2cedf4857d14b3
noobnewbier <peter789b@gmail.com>
2022-01-31 01:10:45 +0900

    feat(debug-console): adding NonebDebugConsole
    NonebDebugConsole interprets any given user input and act accordingly emitting error messages when necessary.

---
+++ Assets/Code/NonebNi/EditorConsole/NonebDebugConsole.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using NonebNi.EditorConsole.Parsers;
+
+namespace NonebNi.EditorConsole
+{
+    public class NonebDebugConsole
+    {
+        private readonly CommandHandler _commandHandler;
+        private readonly TextLexer _lexer;
+        private readonly ExpressionParser _parser;
+
+        public StringBuilder AccumulatedOutput { get; } = new StringBuilder();
+
+        public NonebDebugConsole(CommandHandler commandHandler, ExpressionParser parser, TextLexer lexer)
+        {
+            _commandHandler = commandHandler;
+            _parser = parser;
+            _lexer = lexer;
+        }
+
+        public void InterpretInput(string input)
+        {
+            var expressions = _lexer.Lex(input);
+            var command = _parser.Parse(expressions);
+
+            _commandHandler.Handle(command, AccumulatedOutput);
+        }
+    }
+}
