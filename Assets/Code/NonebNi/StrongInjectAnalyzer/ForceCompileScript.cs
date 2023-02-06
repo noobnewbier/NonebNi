e9da811bd60a129d5c943210c36fa31d80815288
noobnewbier <peter789b@gmail.com>
2023-02-06 20:19:45 +0000

    chore: adding strong inject

---
+++ Assets/Code/NonebNi/StrongInjectAnalyzer/ForceCompileScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace NonebNi.StrongInjectAnalyzer
+{
+    public class ForceCompileScript : MonoBehaviour
+    {
+        /*
+     * Deliberately Empty - Just needed to force Unity to compile this assembly and hence analyze any other asmdef that references this assembly
+     */
+    }
+}
