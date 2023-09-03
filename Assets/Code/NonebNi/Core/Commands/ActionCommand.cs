6d1e26369873afd15cfbfbe76a80e590adb390e4
noobnewbier <peter789b@gmail.com>
2023-09-03 20:00:11 +0100

    feat(action-system): adding ActionCommand and its relevant class.
    - MoveUnitCommand is removed, new "move" action will be added in favour of the new system.

---
+++ Assets/Code/NonebNi/Core/Commands/ActionCommand.cs
@@ -0,0 +1,20 @@
+﻿using NonebNi.Core.Actions;
+using NonebNi.Core.Coordinates;
+using NonebNi.Core.Entities;
+
+namespace NonebNi.Core.Commands
+{
+    public class ActionCommand : ICommand
+    {
+        public readonly Action Action;
+        public readonly EntityData ActorEntity;
+        public readonly Coordinate TargetCoord;
+
+        public ActionCommand(Action action, EntityData actorEntity, Coordinate targetCoord)
+        {
+            Action = action;
+            ActorEntity = actorEntity;
+            TargetCoord = targetCoord;
+        }
+    }
+}
---
+++ Assets/Code/NonebNi/Core/Commands/Handlers/ActionCommandHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NonebNi.Core.Actions;
+using NonebNi.Core.Coordinates;
+using NonebNi.Core.Entities;
+using NonebNi.Core.Maps;
+using NonebNi.Core.Sequences;
+
+namespace NonebNi.Core.Commands.Handlers
+{
+    public class ActionCommandHandler : ICommandHandler<ActionCommand>
+    {
+        private readonly IMap _map;
+        private readonly ITargetValidityChecker _targetValidityChecker;
+
+        public ActionCommandHandler(IMap map, ITargetValidityChecker targetValidityChecker)
+        {
+            _map = map;
+            _targetValidityChecker = targetValidityChecker;
+        }
+
+        public IEnumerable<ISequence> Evaluate(ActionCommand command)
+        {
+            return command.Action.Effects.SelectMany(
+                e =>
+                {
+                    var targets = FindTargets(
+                        command.ActorEntity,
+                        command.TargetCoord,
+                        command.Action.TargetArea,
+                        command.Action.TargetRestriction
+                    );
+                    return e.Evaluate(_map, command.ActorEntity, targets);
+                }
+            );
+        }
+
+        private IEnumerable<IActionTarget> FindTargets(
+            EntityData actor,
+            Coordinate targetCoord,
+            TargetArea targetArea,
+            TargetRestriction restriction)
+        {
+            foreach (var coord in GetTargetedCoordinates(actor, targetCoord, targetArea))
+            {
+                var affectedEntity = GetAffectedEntity(actor, targetCoord, restriction);
+                if (affectedEntity == null)
+                    yield return coord;
+                else
+                    yield return affectedEntity;
+            }
+        }
+
+        private IEnumerable<Coordinate> GetTargetedCoordinates(
+            EntityData actor,
+            Coordinate targetCoord,
+            TargetArea targetArea)
+        {
+            switch (targetArea)
+            {
+                case TargetArea.Single:
+                    yield return targetCoord;
+                    break;
+                case TargetArea.Fan:
+                    if (_map.TryFind(actor, out Coordinate actorCoord)) yield break;
+
+                    var relativeCoord = targetCoord - actorCoord;
+                    yield return targetCoord;
+                    yield return relativeCoord.RotateLeft();
+                    yield return relativeCoord.RotateRight();
+
+                    break;
+                case TargetArea.Circle:
+                    foreach (var neighbour in targetCoord.Neighbours)
+                        if (_map.IsCoordinateWithinMap(neighbour))
+                            yield return neighbour;
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(targetArea), targetArea, null);
+            }
+        }
+
+        private EntityData? GetAffectedEntity(
+            EntityData caster,
+            Coordinate targetCoord,
+            TargetRestriction targetRestriction)
+        {
+            if (!_map.TryGet(targetCoord, out EntityData? target)) return null;
+            if (!_targetValidityChecker.IsPassingTargetRestrictionCheck(targetRestriction, caster, targetCoord))
+                return null;
+
+            return target;
+        }
+    }
+}
