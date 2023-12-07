bb733e564e6a70e1b114d5c557b16bb4b9446d0d
noobnewbier <peter789b@gmail.com>
2023-12-07 19:59:31 +0000

    fix(core): fix a bug where Node can't be correctly serialized for all entity data.

--- Assets/Code/NonebNi/Core/Maps/Node.cs
+++ Assets/Code/NonebNi/Core/Maps/Node.cs
@@ -14,22 +14,22 @@ namespace NonebNi.Core.Maps
     ///     For any type of <see cref="EntityData" />, a node can only hold at most 1 of it.
     /// </summary>
     [Serializable] //Unity doesn't support polymorphism + Serialization, we need to hand craft it
-    public partial class Node
+    public class Node
     {
         [SerializeField] private TileData tileData;
 
-        private List<EntityData> _entityDatas;
+        [SerializeReference] private List<EntityData> entityDatas;
 
         public Node(TileData tileData)
         {
             this.tileData = tileData;
-            _entityDatas = new List<EntityData>();
+            entityDatas = new List<EntityData>();
         }
 
         public Node(TileData tileData, List<EntityData> entityDatas)
         {
             this.tileData = tileData;
-            _entityDatas = entityDatas;
+            this.entityDatas = entityDatas;
         }
 
         public TileData TileData
@@ -42,18 +42,18 @@ namespace NonebNi.Core.Maps
             }
         }
 
-        public T? Get<T>() where T : EntityData => _entityDatas.OfType<T>().FirstOrDefault();
-        public bool Has<T>(T entityData) where T : EntityData => _entityDatas.Contains(entityData);
+        public T? Get<T>() where T : EntityData => entityDatas.OfType<T>().FirstOrDefault();
+        public bool Has<T>(T entityData) where T : EntityData => entityDatas.Contains(entityData);
 
         public void Set<T>(T? entityData) where T : EntityData
         {
             if (typeof(T) == typeof(EntityData))
                 Debug.LogWarning(
-                    $"I am fairly certain you don't want to nuke everything in the {nameof(_entityDatas)}, future me are you sure this is what you want?"
+                    $"I am fairly certain you don't want to nuke everything in the {nameof(entityDatas)}, future me are you sure this is what you want?"
                 );
 
-            _entityDatas.RemoveAll(e => e is T);
-            if (entityData != null) _entityDatas.Add(entityData);
+            entityDatas.RemoveAll(e => e is T);
+            if (entityData != null) entityDatas.Add(entityData);
         }
     }
 }
