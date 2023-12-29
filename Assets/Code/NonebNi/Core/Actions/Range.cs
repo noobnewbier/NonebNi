dd9e90305275420b9e553d2c96e2672496dc60ea
noobnewbier <peter789b@gmail.com>
2023-12-29 03:09:31 +0000

    refactor(action-system): ConstantRange is now it its own file

--- Assets/Code/NonebNi/Core/Actions/Range.cs
+++ Assets/Code/NonebNi/Core/Actions/Range.cs
@@ -8,16 +8,4 @@ namespace NonebNi.Core.Actions
 
         public static implicit operator Range(int range) => new ConstantRange(range);
     }
-
-    public class ConstantRange : Range
-    {
-        private readonly int _range;
-
-        public ConstantRange(int range)
-        {
-            _range = range;
-        }
-
-        public override int CalculateRange(EntityData _) => _range;
-    }
 }
