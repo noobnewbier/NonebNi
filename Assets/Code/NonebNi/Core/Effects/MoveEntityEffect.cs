0524f48eebcba5864f1762b49bad1465bf4d29cd
noobnewbier <peter789b@gmail.com>
2023-12-29 03:14:40 +0000

    feat(action-system): added MoveEntityEffect to let effect move another effect

---
+++ Assets/Code/NonebNi/Core/Effects/MoveEntityEffect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NonebNi.Core.Actions;
+using NonebNi.Core.Coordinates;
+using NonebNi.Core.Entities;
+using NonebNi.Core.Maps;
+using NonebNi.Core.Sequences;
+using Unity.Logging;
+
+namespace NonebNi.Core.Effects
+{
+    [Serializable]
+    public class MoveEntityEffect : Effect
+    {
+        protected override IEnumerable<ISequence> OnEvaluate(
+            IMap map,
+            EntityData actionCaster,
+            IEnumerable<IActionTarget> targets)
+        {
+            targets = targets.ToArray();
+            if (targets.Count() != 2)
+                Log.Error($"{nameof(MoveEntityEffect)} expects 2 and exactly 2 parameter - something went wrong.");
+
+            if (targets.FirstOrDefault() is not EntityData targetEntity)
+            {
+                Log.Error($"{nameof(MoveEntityEffect)} without an entity to move makes no sense!");
+                yield break;
+            }
+
+            if (targets.ElementAtOrDefault(1) is not Coordinate targetCoord)
+            {
+                Log.Error($"{nameof(MoveEntityEffect)} without a coordinate as target position makes no sense!");
+                yield break;
+            }
+
+            var result = map.Move(targetEntity, targetCoord);
+
+            if (result == MoveResult.Success) yield return new MoveSequence(targetEntity, targetCoord);
+        }
+    }
+}
