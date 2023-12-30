20c6ee0bc6a5489c072fa12269817ea051785ff8
noobnewbier <peter789b@gmail.com>
2023-12-30 19:19:42 +0000

    fix(action-system): reformatting action datas for readability again

--- Assets/Code/NonebNi/Core/Actions/ActionDatas.cs
+++ Assets/Code/NonebNi/Core/Actions/ActionDatas.cs
@@ -1,4 +1,6 @@
 ﻿using NonebNi.Core.Effects;
+using static NonebNi.Core.Actions.TargetArea;
+using static NonebNi.Core.Actions.TargetRestriction;
 
 namespace NonebNi.Core.Actions
 {
@@ -10,8 +12,8 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction Move = new(
             "move",
             new StatBasedRange(1f, StatBasedRange.StatType.Speed),
-            TargetRestriction.NonOccupied,
-            TargetArea.Single,
+            NonOccupied,
+            Single,
             0,
             new MoveEffect()
         );
@@ -19,8 +21,8 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction Bash = new(
             "bash",
             1,
-            TargetRestriction.Enemy,
-            TargetArea.Single,
+            Enemy,
+            Single,
             1,
             new KnockBackEffect(1)
         );
@@ -28,8 +30,8 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction Shoot = new(
             "shoot",
             WeaponBasedRange,
-            TargetRestriction.Enemy,
-            TargetArea.Single,
+            Enemy,
+            Single,
             1,
             new DamageEffect(new StatBasedDamage(1f, StatBasedDamage.StatType.Focus))
         );
@@ -37,8 +39,8 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction PowerShot = new(
             "power-shot",
             WeaponBasedRange,
-            TargetRestriction.Enemy | TargetRestriction.ClearPath,
-            TargetArea.Single,
+            Enemy | ClearPath,
+            Single,
             1,
             new DamageEffect(new StatBasedDamage(1f, StatBasedDamage.StatType.Focus)),
             new KnockBackEffect(1)
@@ -47,8 +49,8 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction Strike = new(
             "strike",
             1,
-            TargetRestriction.Enemy,
-            TargetArea.Single,
+            Enemy,
+            Single,
             1,
             new DamageEffect(new StatBasedDamage(1f, StatBasedDamage.StatType.Strength))
         );
@@ -56,8 +58,8 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction Swing = new(
             "swing",
             1,
-            TargetRestriction.Enemy,
-            TargetArea.Fan,
+            Enemy,
+            Fan,
             1,
             new DamageEffect(new StatBasedDamage(1f, StatBasedDamage.StatType.Strength)),
             new KnockBackEffect(1)
@@ -66,8 +68,8 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction Slash = new(
             "slash",
             1,
-            TargetRestriction.Enemy,
-            TargetArea.Single,
+            Enemy,
+            Single,
             1,
             new DamageEffect(new StatBasedDamage(1f, StatBasedDamage.StatType.Focus))
         );
@@ -77,10 +79,10 @@ namespace NonebNi.Core.Actions
             new StatBasedRange(1, StatBasedRange.StatType.Focus),
             new[]
             {
-                TargetRestriction.Friendly,
-                TargetRestriction.NonOccupied
+                Friendly,
+                NonOccupied
             },
-            TargetArea.Single,
+            Single,
             1,
             new MoveEntityEffect()
         );
@@ -88,21 +90,17 @@ namespace NonebNi.Core.Actions
         public static readonly NonebAction Lure = new(
             "lure",
             new StatBasedRange(1, StatBasedRange.StatType.Focus),
-            TargetRestriction.Enemy | TargetRestriction.ClearPath | TargetRestriction.FirstTileToTargetDirectionIsEmpty,
-            TargetArea.Single,
+            Enemy | ClearPath | FirstTileToTargetDirectionIsEmpty,
+            Single,
             1,
             new PullEntityEffect()
         );
-        
+
         public static readonly NonebAction Vault = new(
             "vault",
             1,
-            new[]
-            {
-                TargetRestriction.Occupied | TargetRestriction.TargetCoordPlusDirectionToTargetIsEmpty |
-                TargetRestriction.IsCoordinate
-            },
-            TargetArea.Single,
+            Occupied | TargetCoordPlusDirectionToTargetIsEmpty | IsCoordinate,
+            Single,
             1,
             new MoveOverEffect()
         );
@@ -112,10 +110,10 @@ namespace NonebNi.Core.Actions
             new Range[] { 2, 1 },
             new[]
             {
-                TargetRestriction.Enemy,
-                TargetRestriction.NonOccupied
+                Enemy,
+                NonOccupied
             },
-            TargetArea.Single,
+            Single,
             1,
             new MoveEntityEffect()
         );
