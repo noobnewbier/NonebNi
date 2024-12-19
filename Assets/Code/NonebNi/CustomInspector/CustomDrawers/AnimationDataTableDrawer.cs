2ae1a2ebcaece574a99d9d76d5076b1e88f85815
noobnewbier <peter789b@gmail.com>
2024-12-19 01:52:52 +0000

    feat(editor): adding AnimationDataTableDrawer as the nesting bothers me

---
+++ Assets/Code/NonebNi/CustomInspector/CustomDrawers/AnimationDataTableDrawer.cs
@@ -0,0 +1,32 @@
+﻿using NonebNi.Ui.Animation;
+using UnityEditor;
+using UnityEngine;
+using UnityUtils.Editor;
+
+namespace NonebNi.CustomInspector.CustomDrawers
+{
+    [CustomPropertyDrawer(typeof(AnimationDataTable))]
+    public class AnimationDataTableDrawer : PropertyDrawer
+    {
+        private SerializedProperty? _animIdAndDataProperty;
+
+        private void Init(SerializedProperty property)
+        {
+            _animIdAndDataProperty ??= property.NFindPropertyRelative("animIdAndData")!;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Init(property);
+
+            EditorGUI.PropertyField(position, _animIdAndDataProperty, label);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            Init(property);
+
+            return EditorGUI.GetPropertyHeight(_animIdAndDataProperty, label);
+        }
+    }
+}
