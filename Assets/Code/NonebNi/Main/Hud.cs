a8c3ceefe6bd7e0b6f4f15afbd8ccd2d7fe0bc70
noobnewbier <peter789b@gmail.com>
2025-01-14 22:40:24 +0000

    chore(ui): obsoleting Hud

--- Assets/Code/NonebNi/Main/Hud.cs
+++ Assets/Code/NonebNi/Main/Hud.cs
@@ -1,4 +1,5 @@
-﻿using NonebNi.Core.Agents;
+﻿using System;
+using NonebNi.Core.Agents;
 using NonebNi.Core.Level;
 using NonebNi.Ui.Huds;
 using UnityEngine;
@@ -6,10 +7,10 @@ using UnityEngine.Serialization;
 
 namespace NonebNi.Main
 {
+    [Obsolete("to be replaced by new ui")]
     public class Hud : MonoBehaviour
     {
-        [FormerlySerializedAs("levelNameView")] [SerializeField]
-        private PrototypeView prototypeView = null!;
+        [FormerlySerializedAs("levelNameView"), SerializeField]  private PrototypeView prototypeView = null!;
 
 
         public void Init(LevelData levelData, IPlayerAgent playerAgent)
