b5e27495754887ea0ee25319098cefb736e2ce83
noobnewbier <peter789b@gmail.com>
2025-01-14 22:39:43 +0000

    feat(ui-core): adding input system

---
+++ Assets/Code/Noneb.UI/Runtime/InputSystems/Event/AnyInput.cs
@@ -0,0 +1,12 @@
+﻿namespace Noneb.UI.InputSystems.Event
+{
+    /// <summary>
+    /// I will be honest, I am not entirely sure why I have this. I think it's to respond to console input?
+    /// It looks like "something Q-Games would have done" plus "younger peter trying to add his own idea"...
+    /// Anyway, future me this is something you need to figure out.
+    /// </summary>
+    public class AnyInput : InputEvent
+    {
+        public static AnyInput Instance { get; } = new();
+    }
+}
---
+++ Assets/Code/Noneb.UI/Runtime/InputSystems/IInputSystem.cs
@@ -0,0 +1,13 @@
+﻿using Noneb.UI.InputSystems.Event;
+using Vector3 = UnityEngine.Vector3;
+
+namespace Noneb.UI.InputSystems
+{
+    public interface IInputSystem
+    {
+        Vector3 MousePosition { get; }
+
+        //TODO: can't remember the me 4 years ago why am I having this?
+        event NEventHandler<AnyInput> AnyInput;
+    }
+}
