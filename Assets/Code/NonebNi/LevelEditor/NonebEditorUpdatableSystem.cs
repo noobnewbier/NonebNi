ad6e17a0f6a4f308e7dde76fe694e7a909ecfcba
noobnewbier <peter789b@gmail.com>
2025-01-14 20:10:45 +0000

    refactor(editor): emphasizing my custom update system is currently only for editor

---
+++ Assets/Code/NonebNi/LevelEditor/NonebEditorUpdatableSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityUtils;
+
+namespace NonebNi.LevelEditor
+{
+    public class NonebEditorUpdatableSystem
+    {
+        public static event Action? Update;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void Initialize()
+        {
+            PlayerLoopHelpers.AddRunnerToPlayerLoop(typeof(Update), typeof(NonebEditorUpdatableSystem), UpdateFunc);
+        }
+
+        private static void UpdateFunc()
+        {
+            Update?.Invoke();
+        }
+    }
+}
