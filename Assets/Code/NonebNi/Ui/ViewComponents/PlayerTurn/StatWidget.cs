9385220782c0ca1060f2a031a4524110cbea99f7
noobnewbier <peter789b@gmail.com>
2025-01-18 17:06:51 +0000

    feat(ui): adding unit stat panel

---
+++ Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/StatWidget.cs
@@ -0,0 +1,29 @@
+﻿using NonebNi.Core.Stats;
+using TMPro;
+using UnityEngine;
+
+namespace NonebNi.Ui.ViewComponents.PlayerTurn
+{
+    public class StatWidget : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI valueText = null!;
+
+        public void Show(int value)
+        {
+            valueText.text = value.ToString();
+        }
+
+        public void Show(int value, int maxValue)
+        {
+            valueText.text = $"{value}/{maxValue}";
+        }
+
+        public void Show(Stat stat)
+        {
+            if (stat.HasMaxLimit)
+                Show(stat.CurrentValue, stat.MaxValue);
+            else
+                Show(stat.CurrentValue);
+        }
+    }
+}
---
+++ Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/UnitDetailsPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using NonebNi.Core.Units;
+using UnityEngine;
+
+namespace NonebNi.Ui.ViewComponents.PlayerTurn
+{
+    public class UnitDetailsPanel : MonoBehaviour
+    {
+        [SerializeField] private StatWidget healthWidget = null!;
+        [SerializeField] private StatWidget fatigueWidget = null!;
+        [SerializeField] private StatWidget initWidget = null!;
+
+        public async UniTask Show(UnitData data, CancellationToken ct = default)
+        {
+            var (_, health) = data.Stats.FindStat("health");
+            var (_, fatigue) = data.Stats.FindStat("fatigue");
+            var (_, initiative) = data.Stats.FindStat("initiative");
+
+            healthWidget.Show(health);
+            fatigueWidget.Show(fatigue);
+            initWidget.Show(initiative);
+        }
+    }
+}
