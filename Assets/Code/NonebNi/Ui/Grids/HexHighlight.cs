99e045e594536fc6d51ad1ca050b478db551e0d5
noobnewbier <peter789b@gmail.com>
2025-01-31 23:01:06 +0000

    feat(ui): HexHighlight actually draws something now

--- Assets/Code/NonebNi/Ui/Grids/HexHighlight.cs
+++ Assets/Code/NonebNi/Ui/Grids/HexHighlight.cs
@@ -1,9 +1,35 @@
-﻿using UnityEngine;
+﻿using NonebNi.Terrain;
+using UnityEngine;
 
 namespace NonebNi.Ui.Grids
 {
     /// <summary>
-    /// Basically act as a tag now as we don't have complicated behaviour, we can worry later.
+    /// Uses line renderer to draw some lines, but in the future it will be more beautiful(hopefully)
     /// </summary>
-    public class HexHighlight : MonoBehaviour { }
+    public class HexHighlight : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer lineRenderer = null!;
+
+        private float _hexInnerRadius = -1f;
+
+        private void Awake()
+        {
+            if (lineRenderer == null) lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+
+        public void Draw(float targetHexInnerRadius)
+        {
+            if (Mathf.Approximately(_hexInnerRadius, targetHexInnerRadius)) return;
+
+            _hexInnerRadius = targetHexInnerRadius;
+            RedrawHexLines();
+        }
+
+        private void RedrawHexLines()
+        {
+            var corners = HexMaths.FindCorners(_hexInnerRadius);
+            lineRenderer.positionCount = corners.Length;
+            lineRenderer.SetPositions(corners);
+        }
+    }
 }
