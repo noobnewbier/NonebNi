e553aa2bfd39b780cc26d40a505064c93a90a4ea
noobnewbier <peter789b@gmail.com>
2025-02-20 23:09:02 +0000

    feat(ui): you can now get input from PlayerTurnWorldSpaceInputControl now

--- Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/PlayerTurnWorldSpaceInputControl.cs
+++ Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/PlayerTurnWorldSpaceInputControl.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using Noneb.UI.InputSystems;
 using NonebNi.Core.Actions;
@@ -15,33 +17,44 @@ namespace NonebNi.Ui.ViewComponents.PlayerTurn
     public interface IPlayerTurnWorldSpaceInputControl
     {
         Coordinate? FindHoveredCoordinate();
-        UniTask ToTargetSelectionMode(UnitData caster, NonebAction action);
         UniTask ToTileInspectionMode();
+        UniTask<IEnumerable<Coordinate>> GetInputForAction(UnitData caster, NonebAction action, CancellationToken token = default);
         void UpdateTargetSelection();
     }
 
     public class PlayerTurnWorldSpaceInputControl : IPlayerTurnWorldSpaceInputControl
     {
-        private const string TileInspectionHighlightId = "tileInspection";
-        private const string TargetSelectionHighlightId = "targetSelection";
+        //TODO: we need a way to make these a bit more reliable, these are jank... at best even frog is doing better.
+        private const string NormalHighlightId = "normal";
+        private const string TargetHighlightId = "target";
+        private const string AreaHintId = "area-hint";
+
         private readonly ICoordinateAndPositionService _coordinateAndPositionService;
         private readonly Plane _gridPlane;
         private readonly IHexHighlighter _hexHighlighter;
         private readonly IInputSystem _inputSystem;
         private readonly Map _map;
         private readonly Camera _playerViewCamera;
+        private readonly ITargetFinder _targetFinder;
 
         private CancellationTokenSource? _cts;
-        private AsyncLazy? _updateLoopTask;
 
 
-        public PlayerTurnWorldSpaceInputControl(IInputSystem inputSystem, ICoordinateAndPositionService coordinateAndPositionService, TerrainConfigData terrainConfigData, Camera playerViewCamera, Map map, IHexHighlighter hexHighlighter)
+        public PlayerTurnWorldSpaceInputControl(
+            IInputSystem inputSystem,
+            ICoordinateAndPositionService coordinateAndPositionService,
+            TerrainConfigData terrainConfigData,
+            Camera playerViewCamera,
+            Map map,
+            IHexHighlighter hexHighlighter,
+            ITargetFinder targetFinder)
         {
             _inputSystem = inputSystem;
             _coordinateAndPositionService = coordinateAndPositionService;
             _playerViewCamera = playerViewCamera;
             _map = map;
             _hexHighlighter = hexHighlighter;
+            _targetFinder = targetFinder;
             _gridPlane = terrainConfigData.GridPlane;
         }
 
@@ -57,15 +70,65 @@ namespace NonebNi.Ui.ViewComponents.PlayerTurn
             return coord;
         }
 
-        public async UniTask ToTargetSelectionMode(UnitData caster, NonebAction action)
+        public async UniTask<IEnumerable<Coordinate>> GetInputForAction(UnitData caster, NonebAction action, CancellationToken token = default)
         {
-            // _hexHighlighter.RemoveHighlight(TileInspectionHighlightId);
-            //
-            // _cts?.Cancel();
-            //
-            //
-            // _cts = new CancellationTokenSource();
-            // _hexHighlighter.RemoveHighlight(TargetSelectionHighlightId);
+            async UniTask<Coordinate?> GetUserInputForRequest((RangeStatus status, Coordinate coord)[] ranges, CancellationToken ct)
+            {
+                Coordinate? inputCoord = null;
+                while (!ct.IsCancellationRequested && inputCoord == null)
+                {
+                    await UniTask.NextFrame();
+
+                    _hexHighlighter.RemoveRequest(HighlightRequestId.TargetSelection);
+                    var coord = FindHoveredCoordinate();
+                    if (coord == null) continue;
+
+                    // Keep showing the highlight - we are good
+                    _hexHighlighter.RequestHighlight(coord, HighlightRequestId.TargetSelection, TargetHighlightId);
+
+                    if (!_inputSystem.LeftClick) continue;
+
+
+                    (RangeStatus status, Coordinate _)? matchingRangeStatus = ranges.FirstOrDefault(t => t.coord == coord);
+                    if (matchingRangeStatus is not { status: RangeStatus.Targetable })
+                        //todo: signal invalid input - potentially audio and even a tooltip to explain why shit is wrong
+                        continue;
+
+                    //todo: highlight color need to change depending on if the selection is valid.
+                    inputCoord = coord;
+                }
+
+                _hexHighlighter.RemoveRequest(HighlightRequestId.AreaHint);
+                return inputCoord;
+            }
+
+            async UniTask<IEnumerable<Coordinate>> Do(CancellationToken ct)
+            {
+                _hexHighlighter.ClearAll();
+
+                var playerInputs = new List<Coordinate>();
+                foreach (var request in action.TargetRequests)
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    var ranges = _targetFinder.FindRange(caster, request).ToArray();
+                    _hexHighlighter.RemoveRequest(HighlightRequestId.AreaHint);
+                    _hexHighlighter.RequestHighlight(ranges.Select(r => r.coord), HighlightRequestId.AreaHint, AreaHintId);
+
+                    var input = await GetUserInputForRequest(ranges, ct);
+                    if (input != null) playerInputs.Add(input);
+                }
+
+                _hexHighlighter.RemoveRequest(HighlightRequestId.TargetSelection, HighlightRequestId.AreaHint);
+
+                return playerInputs;
+            }
+
+            _cts?.Cancel();
+            _cts = CancellationTokenSource.CreateLinkedTokenSource(token);
+            var coordinates = await Do(_cts.Token);
+
+            return coordinates;
         }
 
         public async UniTask ToTileInspectionMode()
