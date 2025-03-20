f94a7b180e9dc971dcdb89207dfe9c664e82b83f
noobnewbier <peter789b@gmail.com>
2025-03-20 21:05:06 +0000

    feat(ui): UnitDetailsPanel now show the speed stat as well

--- Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/UnitDetailsPanel.cs
+++ Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/UnitDetailsPanel.cs
@@ -11,6 +11,7 @@ namespace NonebNi.Ui.ViewComponents.PlayerTurn
         [SerializeField] private StatWidget healthWidget = null!;
         [SerializeField] private StatWidget fatigueWidget = null!;
         [SerializeField] private StatWidget initWidget = null!;
+        [SerializeField] private StatWidget speedWidget = null!;
         [SerializeField] private TextMeshProUGUI nameLabel = null!;
 
         public async UniTask Show(UnitData data, CancellationToken ct = default)
@@ -18,11 +19,13 @@ namespace NonebNi.Ui.ViewComponents.PlayerTurn
             var (_, health) = data.Stats.FindStat("health");
             var (_, fatigue) = data.Stats.FindStat("fatigue");
             var (_, initiative) = data.Stats.FindStat("initiative");
+            var (_, speed) = data.Stats.FindStat("speed");
 
             nameLabel.text = data.Name;
             healthWidget.Show(health);
             fatigueWidget.Show(fatigue);
             initWidget.Show(initiative);
+            speedWidget.Show(speed);
         }
     }
 }
