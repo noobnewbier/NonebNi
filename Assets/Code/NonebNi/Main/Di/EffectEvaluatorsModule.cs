ca08aee09a72689cf9139f67bad491a6e7aa088b
noobnewbier <peter789b@gmail.com>
2025-03-20 21:03:12 +0000

    chore: style changes as the list is too long and I can't read it

--- Assets/Code/NonebNi/Main/Di/EffectEvaluatorsModule.cs
+++ Assets/Code/NonebNi/Main/Di/EffectEvaluatorsModule.cs
@@ -4,6 +4,15 @@ using StrongInject.Modules;
 
 namespace NonebNi.Main.Di
 {
-    [RegisterModule(typeof(CollectionsModule)), Register(typeof(DamageEffect.Evaluator), typeof(IEffectEvaluator)), Register(typeof(KnockBackEffect.Evaluator), typeof(IEffectEvaluator)), Register(typeof(MoveEffect.Evaluator), typeof(IEffectEvaluator)), Register(typeof(MoveEntityEffect.Evaluator), typeof(IEffectEvaluator)), Register(typeof(MoveOverEffect.Evaluator), typeof(IEffectEvaluator)), Register(typeof(PullEntityEffect.Evaluator), typeof(IEffectEvaluator)), Register(typeof(SwapPositionEffect.Evaluator), typeof(IEffectEvaluator))]
+    [
+        RegisterModule(typeof(CollectionsModule)),
+        Register(typeof(DamageEffect.Evaluator), typeof(IEffectEvaluator)),
+        Register(typeof(KnockBackEffect.Evaluator), typeof(IEffectEvaluator)),
+        Register(typeof(MoveEffect.Evaluator), typeof(IEffectEvaluator)),
+        Register(typeof(MoveEntityEffect.Evaluator), typeof(IEffectEvaluator)),
+        Register(typeof(MoveOverEffect.Evaluator), typeof(IEffectEvaluator)),
+        Register(typeof(PullEntityEffect.Evaluator), typeof(IEffectEvaluator)),
+        Register(typeof(SwapPositionEffect.Evaluator), typeof(IEffectEvaluator))
+    ]
     public class EffectEvaluatorsModule { }
 }
