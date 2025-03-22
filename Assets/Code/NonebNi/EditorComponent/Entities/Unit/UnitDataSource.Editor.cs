40fa5c5a58d21deb84fb333c02dd2efef585565a
noobnewbier <peter789b@gmail.com>
2025-03-22 01:00:20 +0000

    refactor(editor): extracting editor code in partial class for sanity

---
+++ Assets/Code/NonebNi/EditorComponent/Entities/Unit/UnitDataSource.Editor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using NonebNi.Core.Actions;
+using NonebNi.CustomInspector;
+using UnityEditor;
+using UnityEngine;
+
+namespace NonebNi.EditorComponent.Entities.Unit
+{
+    public partial class UnitDataSource
+    {
+        [SerializeField] private EditorData data = new();
+
+        [Serializable]
+        public class EditorData { }
+
+        [CustomEditor(typeof(UnitDataSource))]
+        private class InsideEditor : Editor
+        {
+            private NonebGUIDrawer _drawer = null!;
+            private UnitDataSource _self = null!;
+
+            private void OnEnable()
+            {
+                _drawer = new NonebGUIDrawer(serializedObject);
+                _self = (UnitDataSource)target;
+            }
+
+            public override void OnInspectorGUI()
+            {
+                _drawer.Update();
+
+                _drawer.DrawProperty(nameof(health));
+                _drawer.DrawProperty(nameof(maxHealth));
+                _drawer.DrawAutoCompleteProperty(
+                    nameof(actionIds),
+                    () => ActionDatas.Actions.Select(a => a.Id).ToArray()
+                );
+                _drawer.DrawProperty(nameof(initiative));
+                _drawer.DrawProperty(nameof(speed));
+                _drawer.DrawProperty(nameof(focus));
+                _drawer.DrawProperty(nameof(strength));
+                _drawer.DrawProperty(nameof(armor));
+                _drawer.DrawProperty(nameof(weaponRange));
+                _drawer.DrawProperty(nameof(fatigue));
+                _drawer.DrawProperty(nameof(maxFatigue));
+
+                _drawer.Apply();
+            }
+        }
+    }
+}
--- Assets/Code/NonebNi/EditorComponent/Entities/Unit/UnitDataSource.cs
+++ Assets/Code/NonebNi/EditorComponent/Entities/Unit/UnitDataSource.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using NonebNi.Core.Actions;
 using NonebNi.Core.Units;
-using NonebNi.CustomInspector;
 using Unity.Logging;
-using UnityEditor;
 using UnityEngine;
 using UnityUtils.Constants;
 
 namespace NonebNi.EditorComponent.Entities.Unit
 {
     [Serializable, CreateAssetMenu(fileName = nameof(UnitDataSource), menuName = MenuName.Data + nameof(UnitDataSource))]
-    public class UnitDataSource : EditorEntityDataSource<EditorEntityData<UnitData>>
+    public partial class UnitDataSource : EditorEntityDataSource<EditorEntityData<UnitData>>
     {
         [SerializeField] private int health;
         [SerializeField] private int maxHealth;
@@ -64,46 +62,6 @@ namespace NonebNi.EditorComponent.Entities.Unit
         }
 
 #if UNITY_EDITOR
-
-        [Serializable]
-        public class EditorData { }
-
-        [SerializeField] private EditorData data = new();
-
-        [CustomEditor(typeof(UnitDataSource))]
-        private class InsideEditor : Editor
-        {
-            private NonebGUIDrawer _drawer = null!;
-            private UnitDataSource _self = null!;
-
-            private void OnEnable()
-            {
-                _drawer = new NonebGUIDrawer(serializedObject);
-                _self = (UnitDataSource)target;
-            }
-
-            public override void OnInspectorGUI()
-            {
-                _drawer.Update();
-
-                _drawer.DrawProperty(nameof(health));
-                _drawer.DrawProperty(nameof(maxHealth));
-                _drawer.DrawAutoCompleteProperty(
-                    nameof(actionIds),
-                    () => ActionDatas.Actions.Select(a => a.Id).ToArray()
-                );
-                _drawer.DrawProperty(nameof(initiative));
-                _drawer.DrawProperty(nameof(speed));
-                _drawer.DrawProperty(nameof(focus));
-                _drawer.DrawProperty(nameof(strength));
-                _drawer.DrawProperty(nameof(armor));
-                _drawer.DrawProperty(nameof(weaponRange));
-                _drawer.DrawProperty(nameof(fatigue));
-                _drawer.DrawProperty(nameof(maxFatigue));
-
-                _drawer.Apply();
-            }
-        }
 
 #endif
     }
