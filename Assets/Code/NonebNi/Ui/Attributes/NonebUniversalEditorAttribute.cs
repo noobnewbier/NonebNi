aa0d810d8bcb5e94183f55472bf4ab8250885a5d
noobnewbier <peter789b@gmail.com>
2025-04-15 21:38:41 +0100

    feat(editor): adding an universal inspector that I can share common functionality

---
+++ Assets/Code/NonebNi/Ui/Attributes/NonebUniversalEditorAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace NonebNi.Ui.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class NonebUniversalEditorAttribute : Attribute { }
+}
