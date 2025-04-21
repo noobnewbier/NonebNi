a75bfda960e8410ef6f9224251e253845d363572
noobnewbier <peter789b@gmail.com>
2025-04-21 18:15:58 +0100

    refactor(core): making sure some logging is done when EffectTargetGroup.AsSingleTarget might not be doing what the user expects

--- Assets/Code/NonebNi/Core/Effects/EffectTargetGroup.cs
+++ Assets/Code/NonebNi/Core/Effects/EffectTargetGroup.cs
@@ -1,10 +1,27 @@
 ﻿using System.Linq;
 using NonebNi.Core.Actions;
+using Unity.Logging;
 
 namespace NonebNi.Core.Effects
 {
     public record EffectTargetGroup(IActionTarget[] Targets)
     {
-        public IActionTarget? AsSingleTarget => Targets.FirstOrDefault();
+        public IActionTarget? AsSingleTarget
+        {
+            get
+            {
+                switch (Targets.Length)
+                {
+                    case > 1:
+                        Log.Warning("Multiple targets found when the code really just wanted one target, something went wrong");
+                        break;
+                    case 0:
+                        Log.Warning("My gut feeling is that you should never be here, I can't really fathom why would this be a valid case right now, but maybe I was wrong, future me glhf");
+                        break;
+                }
+
+                return Targets.FirstOrDefault();
+            }
+        }
     }
 }
