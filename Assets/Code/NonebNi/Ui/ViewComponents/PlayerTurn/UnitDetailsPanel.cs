1b0117a878115bf2ff93955c1bed5a922cfe0efa
noobnewbier <peter789b@gmail.com>
2025-04-21 18:18:15 +0100

    fix(ui): making sure UI update as unit's stat changes

--- Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/UnitDetailsPanel.cs
+++ Assets/Code/NonebNi/Ui/ViewComponents/PlayerTurn/UnitDetailsPanel.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading;
-using Cysharp.Threading.Tasks;
-using NonebNi.Core.Stats;
+﻿using NonebNi.Core.Stats;
 using NonebNi.Core.Units;
 using TMPro;
 using UnityEngine;
@@ -17,16 +15,35 @@ namespace NonebNi.Ui.ViewComponents.PlayerTurn
         [SerializeField] private TextMeshProUGUI nameLabel = null!;
         [SerializeField] private TextMeshProUGUI factionLabel = null!;
 
-        public async UniTask Show(UnitData data, CancellationToken ct = default)
-        {
-            var (_, health) = data.Stats.FindStat(StatId.Health);
-            var (_, fatigue) = data.Stats.FindStat(StatId.Fatigue);
-            var (_, initiative) = data.Stats.FindStat(StatId.Initiative);
-            var (_, speed) = data.Stats.FindStat(StatId.Speed);
-            var (_, actionPoint) = data.Stats.FindStat(StatId.ActionPoint);
+        private UnitData? _currentUnit;
 
-            nameLabel.text = data.Name.GetLocalized();
-            factionLabel.text = data.FactionId;
+        public void Show(UnitData data)
+        {
+            //TODO: need unsubscribe before exiting
+            if (_currentUnit != null) _currentUnit.Stats.StatUpdated -= OnStatUpdated;
+
+            _currentUnit = data;
+            _currentUnit.Stats.StatUpdated += OnStatUpdated;
+            Refresh();
+        }
+
+        private void OnStatUpdated(Stat stat)
+        {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (_currentUnit == null) return;
+
+            var (_, health) = _currentUnit.Stats.FindStat(StatId.Health);
+            var (_, fatigue) = _currentUnit.Stats.FindStat(StatId.Fatigue);
+            var (_, initiative) = _currentUnit.Stats.FindStat(StatId.Initiative);
+            var (_, speed) = _currentUnit.Stats.FindStat(StatId.Speed);
+            var (_, actionPoint) = _currentUnit.Stats.FindStat(StatId.ActionPoint);
+
+            nameLabel.text = _currentUnit.Name.GetLocalized();
+            factionLabel.text = _currentUnit.FactionId;
             healthWidget.Show(health);
             fatigueWidget.Show(fatigue);
             initWidget.Show(initiative);
