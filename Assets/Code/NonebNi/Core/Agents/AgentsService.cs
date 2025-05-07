11fe19585a46039a5fba61a13685780db7829e29
noobnewbier <peter789b@gmail.com>
2025-05-07 18:44:22 +0100

    refactor(core): you can now get agent's input as command

--- Assets/Code/NonebNi/Core/Agents/AgentsService.cs
+++ Assets/Code/NonebNi/Core/Agents/AgentsService.cs
@@ -2,34 +2,67 @@
 using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using NonebNi.Core.Commands;
 using NonebNi.Core.Decisions;
+using Unity.Logging;
 using UnityEngine;
 
 namespace NonebNi.Core.Agents
 {
     public interface IAgentsService
     {
-        UniTask<IDecision?> GetAgentDecision(string factionId);
-
         void OverrideDecision(IDecision decision);
+        UniTask<ICommand> GetAgentInput(string factionId);
     }
 
     public class AgentsService : IAgentsService
     {
         private readonly IReadOnlyList<IAgent> _agents;
+        private readonly IDecisionValidator _validator;
 
         private CancellationTokenSource? _getDecisionCts;
         private IDecision? _overridingDecision;
 
-        public AgentsService(IReadOnlyList<IAgent> agents)
+        public AgentsService(IReadOnlyList<IAgent> agents, IDecisionValidator validator)
         {
             if (agents.GroupBy(a => a.Faction).Any(g => g.Count() > 1))
                 Debug.LogError("More than one agent shares the same faction id - this is invalid");
 
             _agents = agents;
+            _validator = validator;
         }
 
-        public async UniTask<IDecision?> GetAgentDecision(string factionId)
+        public async UniTask<ICommand> GetAgentInput(string factionId)
+        {
+            IDecisionValidator.Error? err;
+            ICommand command;
+            do
+            {
+                var decision = await GetAgentDecision(factionId);
+                Log.Info($"[Level] Received Decision: {decision?.GetType()}");
+
+                (err, command) = _validator.ValidateDecision(decision);
+
+                if (err != null) Log.Info($"[Level] Decision Error: {err.Id}, {err.Description}");
+            } while (err != null);
+
+            Log.Info($"[Level] Evaluate Command: {command.GetType()}");
+
+            //TODO: preferrably there is an auto end turn button which ends turn when there's absolutely nothing you can do, hard to implement without being annoying though
+            return command;
+        }
+
+        public void OverrideDecision(IDecision decision)
+        {
+            if (_getDecisionCts == null)
+                //not trying to get a decision atm -> nothing to override.
+                return;
+
+            _overridingDecision = decision;
+            _getDecisionCts.Cancel();
+        }
+
+        private async UniTask<IDecision?> GetAgentDecision(string factionId)
         {
             //resetting the value so the last value won't carry forward to this call
             _getDecisionCts = new CancellationTokenSource();
@@ -42,16 +75,6 @@ namespace NonebNi.Core.Agents
             var agentDecision = await agent.GetDecision(ct);
 
             return _overridingDecision ?? agentDecision;
-        }
-
-        public void OverrideDecision(IDecision decision)
-        {
-            if (_getDecisionCts == null)
-                //not trying to get a decision atm -> nothing to override.
-                return;
-
-            _overridingDecision = decision;
-            _getDecisionCts.Cancel();
         }
     }
 }
