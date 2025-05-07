f75829e9cd26ffbe941d472fd8895c3f8ff63320
noobnewbier <peter789b@gmail.com>
2025-05-07 18:44:47 +0100

    fix(core): PlayerAgent now uses TCS to remove the one frame delay

--- Assets/Code/NonebNi/Core/Agents/PlayerAgent.cs
+++ Assets/Code/NonebNi/Core/Agents/PlayerAgent.cs
@@ -2,11 +2,13 @@
 using Cysharp.Threading.Tasks;
 using NonebNi.Core.Decisions;
 using NonebNi.Core.Factions;
+using Unity.Logging;
 
 namespace NonebNi.Core.Agents
 {
     public interface IPlayerAgent : IAgent
     {
+        //todo: eventually maybe there should be a "brain"(ai brain, vs player brain), and we can only have one type of agent which would be nice 
         void SetDecision(IDecision decision);
     }
 
@@ -15,7 +17,7 @@ namespace NonebNi.Core.Agents
     /// </summary>
     public class PlayerAgent : IPlayerAgent
     {
-        private IDecision? _decision;
+        private UniTaskCompletionSource<IDecision?>? _tcs;
 
         public PlayerAgent(Faction faction)
         {
@@ -24,18 +26,31 @@ namespace NonebNi.Core.Agents
 
         public Faction Faction { get; }
 
-        public async UniTask<IDecision?> GetDecision(CancellationToken ct)
+        public UniTask<IDecision?> GetDecision(CancellationToken ct)
         {
-            _decision = null;
+            //if it breaks, well you got what you deserved
+            if (_tcs?.UnsafeGetStatus() == UniTaskStatus.Pending)
+            {
+                // if we are already waiting, just use the same one dude.
+                ct.Register(() => _tcs.TrySetResult(null)); // still make sure the ct is propagated though
+                return _tcs.Task;
+            }
 
-            await UniTask.WaitUntil(() => _decision != null, cancellationToken: ct).SuppressCancellationThrow();
-
-            return _decision;
+            _tcs = new UniTaskCompletionSource<IDecision?>();
+            ct.Register(() => _tcs.TrySetResult(null));
+            return _tcs.Task;
         }
 
         public void SetDecision(IDecision decision)
         {
-            _decision = decision;
+            if (_tcs == null)
+                // if a tree fell in the forest and no one knows
+                return;
+
+            var tcs = _tcs;
+            _tcs = null; // we want to set this to null before setting the result, that way if there's other chap trying to get decision as the code go on we aren't resetting them
+
+            if (!tcs.TrySetResult(decision)) Log.Error("woah dude this should not have happened");
         }
     }
 }
