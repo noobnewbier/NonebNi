cdc18d3a303d21093906dc458446f5bb4313fad7
noobnewbier <peter789b@gmail.com>
2025-05-07 21:09:37 +0100

    fix(core): reworking the level flow controller to make sure all is working well

--- Assets/Code/NonebNi/Core/FlowControl/LevelFlowController.cs
+++ Assets/Code/NonebNi/Core/FlowControl/LevelFlowController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
 using NonebNi.Core.Agents;
 using NonebNi.Core.Combos;
 using NonebNi.Core.Commands;
-using NonebNi.Core.Decisions;
+using NonebNi.Core.Effects;
 using NonebNi.Core.Units;
 using Unity.Logging;
 
@@ -12,54 +13,67 @@ namespace NonebNi.Core.FlowControl
 {
     public interface ILevelFlowController
     {
+        void Run();
+        void ForcePlayEvent(LevelEvent levelEvent);
+
+        #region Editor Console's dependencies
+
+        //todo: at some point, it would be brill if we can get rid of these, for now though.
+
         IAgentsService AgentsService { get; }
         IActionCommandEvaluator Evaluator { get; }
         IUnitTurnOrderer UnitTurnOrderer { get; }
-        ChannelReader<LevelEvent> Run();
-        void ForcePlayEvent(LevelEvent levelEvent);
+
+        #endregion
     }
 
+    //todo: next big work - combo
+    //todo: combo ui - might be able to reuse quite a bit.
+
+    //todo: could use an error message panel/tooltip even just for debugging
+    //todo: fatigue work upwards only in the ui - it's easier to keep the rest of the maths consistent(going downwards)
+    //todo: action point system? make it play with speed or sth idk
+    //todo: fix I can unintentionally control enemy - (or maybe keep it but make sure it's debug view)
+    //todo: fix order panel
+    //todo: fix starting unit
+    //todo: fix initiative - or leave it as is and be happy with it.
+    //todo: enemy ai, it can be simple raider/wolf ai
+    //todo: at some point we need save/load test.
+    //todo: show cost of an action, either tooltip whatever easiest solution you can come up with.
+    //todo: at some point a better logging system would be nice - but not necessary...?
     public class LevelFlowController : ILevelFlowController
     {
         public LevelFlowController(
             IActionCommandEvaluator evaluator,
             IUnitTurnOrderer unitTurnOrderer,
             IAgentsService agentService,
-            IDecisionValidator decisionValidator,
-            IComboChecker comboChecker)
+            IComboChecker comboChecker,
+            IGameEventControl gameEventControl)
         {
             Evaluator = evaluator;
             UnitTurnOrderer = unitTurnOrderer;
             AgentsService = agentService;
-            DecisionValidator = decisionValidator;
             _comboChecker = comboChecker;
-
-            _eventChannel = Channel.CreateSingleConsumerUnbounded<LevelEvent>();
+            _gameEventControl = gameEventControl;
         }
 
-        public IDecisionValidator DecisionValidator { get; }
         public IAgentsService AgentsService { get; }
         public IActionCommandEvaluator Evaluator { get; }
         public IUnitTurnOrderer UnitTurnOrderer { get; }
 
+        private readonly IGameEventControl _gameEventControl;
         private readonly IComboChecker _comboChecker;
 
-        /// <summary>
-        /// If we need a state machine, we can add one later, atm a simple event queue would suffice
-        /// </summary>
-        /// <returns></returns>
-        private readonly Channel<LevelEvent> _eventChannel;
-
-        public ChannelReader<LevelEvent> Run()
+        public void Run()
         {
             //todo: at some point we need a way to kill it.
             RunLevelFlow().Forget();
-
-            return _eventChannel.Reader;
         }
 
         private async UniTask RunLevelFlow()
         {
+            _gameEventControl.WriteEvent(new LevelEvent.GameStart());
+
             //TODO: replace all these logging w/ a Decorator using StrongInject.
 
             var turnNum = 0; //Mostly for debug purposes - but probably necessary for UI at some point
@@ -72,19 +86,20 @@ namespace NonebNi.Core.FlowControl
                 currentUnit.RestoreActionPoint();
                 currentUnit.RecoverFatigue();
 
-                var newTurn = new LevelEvent.NewTurn(currentUnit);
-                WriteEvent(newTurn);
-
                 while (true)
                 {
+                    var waitForUnitDecision = new LevelEvent.WaitForActiveUnitDecision(currentUnit);
+                    _gameEventControl.WriteEvent(waitForUnitDecision);
+
                     // ReSharper restore RedundantAssignment
-                    var command = await WaitForAgentInput(currentUnit);
-                    bool isDone;
+                    var command = await AgentsService.GetAgentInput(currentUnit.FactionId);
+                    var isDone = false;
+                    var unitKeepActing = false;
                     switch (command)
                     {
                         case ActionCommand actionCommand:
                             await ActionEvaluationFlow(actionCommand, currentUnit);
-                            isDone = false;
+                            unitKeepActing = true;
                             break;
                         case EndTurnCommand:
                         case NullCommand:
@@ -94,6 +109,7 @@ namespace NonebNi.Core.FlowControl
                             throw new ArgumentOutOfRangeException(nameof(command));
                     }
 
+                    if (unitKeepActing) continue;
                     if (isDone) break;
                 }
 
@@ -107,64 +123,42 @@ namespace NonebNi.Core.FlowControl
 
         public void ForcePlayEvent(LevelEvent levelEvent)
         {
-            WriteEvent(levelEvent);
-        }
-
-        private void WriteEvent(LevelEvent levelEvent)
-        {
-            if (!_eventChannel.Writer.TryWrite(levelEvent)) Log.Error("[Level] You should really, never have got here");
+            _gameEventControl.WriteEvent(levelEvent);
         }
 
         private async UniTask ActionEvaluationFlow(ActionCommand command, UnitData currentUnit)
         {
-            EvaluateCommand(command);
-            await ComboFlow(command, currentUnit);
+            var effectedTarget = EvaluateCommand(command);
+            await ComboFlow(effectedTarget, currentUnit);
         }
 
-        private void EvaluateCommand(ActionCommand command)
+        private List<EffectTargetGroup> EvaluateCommand(ActionCommand command)
         {
+            var effectedTarget = Evaluator.FindEffectedTargets(command);
             var actionSequence = Evaluator.Evaluate(command).ToArray();
             var sequenceEvent = new LevelEvent.SequenceOccured(actionSequence);
-            WriteEvent(sequenceEvent);
+            _gameEventControl.WriteEvent(sequenceEvent);
+
+            return effectedTarget;
         }
 
-        private async UniTask ComboFlow(ActionCommand startingCommand, UnitData comboStarter)
+        private async UniTask ComboFlow(List<EffectTargetGroup> previouslyAffectedTargets, UnitData comboStarter)
         {
             // no combo -> nothing to do we can just bugger off
-            var possibleCombos = _comboChecker.FindComboOptions(startingCommand).ToArray();
+            var possibleCombos = _comboChecker.FindComboOptions(previouslyAffectedTargets, comboStarter.FactionId).ToArray();
             if (!possibleCombos.Any()) return;
 
             // wait till the agent give us to something to work on 
             var comboDecisionEvent = new LevelEvent.WaitForComboDecision(comboStarter, possibleCombos);
-            WriteEvent(comboDecisionEvent);
-            if (await WaitForAgentInput(comboStarter) is not ActionCommand actionCommand) return;
+            _gameEventControl.WriteEvent(comboDecisionEvent);
+            if (await AgentsService.GetAgentInput(comboStarter.FactionId) is not ActionCommand actionCommand) return;
 
             // work on that something
-            EvaluateCommand(actionCommand);
+            var targetsAffectedByCombo = EvaluateCommand(actionCommand);
 
             // check if we can actually keep comboing -> combo till heat death if necessary
             if (actionCommand.ActorEntity is not UnitData comboTaker) return;
-            await ComboFlow(actionCommand, comboTaker);
-        }
-
-        private async UniTask<ICommand> WaitForAgentInput(UnitData currentUnit)
-        {
-            IDecisionValidator.Error? err;
-            ICommand command;
-            do
-            {
-                var decision = await AgentsService.GetAgentDecision(currentUnit.FactionId);
-                Log.Info($"[Level] Received Decision: {decision?.GetType()}");
-
-                (err, command) = DecisionValidator.ValidateDecision(decision);
-
-                if (err != null) Log.Info($"[Level] Decision Error: {err.Id}, {err.Description}");
-            } while (err != null);
-
-            Log.Info($"[Level] Evaluate Command: {command.GetType()}");
-
-            //TODO: preferrably there is an auto end turn button which ends turn when there's absolutely nothing you can do, hard to implement without being annoying though
-            return command;
+            await ComboFlow(targetsAffectedByCombo, comboTaker);
         }
     }
 }
