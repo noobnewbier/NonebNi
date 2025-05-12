c47a417eef288486f982eb04eb478147910dd916
noobnewbier <peter789b@gmail.com>
2025-05-12 20:53:45 +0100

    fix(core): removing misleading comment

--- Assets/Code/NonebNi/Core/Commands/ActionCommand.cs
+++ Assets/Code/NonebNi/Core/Commands/ActionCommand.cs
@@ -9,10 +9,6 @@ namespace NonebNi.Core.Commands
     {
         public readonly NonebAction Action;
 
-        /// <summary>
-        /// Actor can be null! This means the action is triggered by something outside of the level,
-        /// this atm only happens with debug command.
-        /// </summary>
         public readonly EntityData ActorEntity;
 
         public readonly IReadOnlyList<Coordinate> TargetCoords;
