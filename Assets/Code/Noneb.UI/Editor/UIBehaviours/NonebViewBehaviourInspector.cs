b659161647e14523f6ac8fb932235a7f6fdf7aff
noobnewbier <peter789b@gmail.com>
2025-07-18 22:31:06 +0100

    feat(editor): adding some minor editor script for debugging

---
+++ Assets/Code/Noneb.UI/Editor/UIBehaviours/NonebViewBehaviourInspector.cs
@@ -0,0 +1,33 @@
+﻿using Noneb.UI.View;
+using NonebNi.CustomInspector;
+using UnityEditor;
+
+namespace Noneb.UI.Editor.UIBehaviours
+{
+    [CustomEditor(typeof(NonebViewBehaviour))]
+    public class NonebViewBehaviourInspector : UnityEditor.Editor
+    {
+        private NonebGUIDrawer _drawer = null!;
+        private NonebViewBehaviour _self = null!;
+
+        private void OnEnable()
+        {
+            _drawer = new NonebGUIDrawer(serializedObject);
+            _self = (NonebViewBehaviour)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            _drawer.Update();
+
+            _drawer.DrawDefaultInspector(this);
+            _drawer.DrawLabel($"IsActive: {_self.IsViewActive}");
+            _drawer.DrawLabel($"InitState: {((INonebView)_self).InitState}");
+
+            _drawer.Apply();
+            Repaint();
+        }
+
+        public class EditorData : ScriptableSingleton<EditorData> { }
+    }
+}
--- Assets/Code/Noneb.UI/Runtime/AssemblyInfo.cs
+++ Assets/Code/Noneb.UI/Runtime/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Noneb.UI.PlayModeTests")]
+[assembly: InternalsVisibleTo("Noneb.UI.Editor")]
+// This assembly is the default dynamic assembly generated Castle DynamicProxy, 
+// used by Moq. Paste in a single line.   
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
