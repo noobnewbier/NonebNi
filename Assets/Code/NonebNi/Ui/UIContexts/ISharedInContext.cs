0ef6fde3958b6ba13131f2115b3cec702d81defc
noobnewbier <peter789b@gmail.com>
2025-07-18 23:31:21 +0100

    feat(core): adding a way to retrieve dependencies without using StrongInject, useful for stuffs like MonoBehaviour

---
+++ Assets/Code/NonebNi/Ui/UIContexts/ISharedInContext.cs
@@ -0,0 +1,22 @@
+﻿namespace NonebNi.Ui.UIContexts
+{
+    public interface ISharedInContext { }
+
+    /// <summary>
+    /// This is weird, but I can't come up with a way to do this locator shenanigans easily
+    /// </summary>
+    public class SharedContextInitializer
+    {
+        private readonly ISharedInContext[] _totInit;
+
+        public SharedContextInitializer(ISharedInContext[] totInit)
+        {
+            _totInit = totInit;
+        }
+
+        public void Init()
+        {
+            foreach (var context in _totInit) UIContext.Set(context.GetType(), context);
+        }
+    }
+}
---
+++ Assets/Code/NonebNi/Ui/UIContexts/UIContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Unity.Logging;
+
+namespace NonebNi.Ui.UIContexts
+{
+    /// <summary>
+    /// KISS - atm it provides dependencies globally(basically Locator),
+    /// but we might move on to a more sophisticated approach later where context can be gameobject/scene independent.
+    /// </summary>
+    public static class UIContext
+    {
+        private static readonly Dictionary<Type, object> Datas = new();
+
+        public static void Set<T>(T data) where T : class
+        {
+            Set(typeof(T), data);
+        }
+
+        public static void Set(Type type, object data)
+        {
+            if (Datas.ContainsKey(type)) Log.Warning("You are overwriting existing context, likely not what you want to do?");
+
+            Datas[type] = data;
+        }
+
+        public static async UniTask<T> Get<T>(CancellationToken ct = default)
+        {
+            while (true)
+            {
+                var (success, data) = Do();
+                if (success) return data!;
+
+                await UniTask.WaitForEndOfFrame(ct);
+            }
+
+            (bool success, T? data) Do()
+            {
+                if (!Datas.TryGetValue(typeof(T), out var data)) return default;
+
+                if (data is not T typedData)
+                {
+                    Log.Error("How the hell did you even end up here");
+                    return default;
+                }
+
+                return (true, typedData);
+            }
+        }
+    }
+}
