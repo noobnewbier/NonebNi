48a510332532e0549ee6016b1e62c67fa185837c
noobnewbier <peter789b@gmail.com>
2025-07-18 23:17:17 +0100

    feat(ui): adding a "root" for our UI so HUD doesn't need to hog all the UI stack

---
+++ Assets/Code/NonebNi/Main/CanvasRoot.cs
@@ -0,0 +1,18 @@
+﻿using Noneb.UI.View;
+using NonebNi.Ui.Attributes;
+using UnityEngine;
+
+namespace NonebNi.Main
+{
+    [NonebUniversalEditor]
+    public class CanvasRoot : MonoBehaviour
+    {
+        private UIStack? _stack;
+
+        public UIStack GetStack()
+        {
+            _stack ??= new UIStack(this);
+            return _stack;
+        }
+    }
+}
