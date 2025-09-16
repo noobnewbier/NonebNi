33b8463d55fdcaf7135bcc2f9ae402dab4a0b95d
noobnewbier <peter789b@gmail.com>
2025-09-16 20:49:07 +0100

    feat(core): added primitive file saving

---
+++ Assets/Code/NonebNi/Core/Serialization/Serializer.cs
@@ -0,0 +1,31 @@
+﻿using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NonebNi.Core.Serialization
+{
+    public interface ISerializer
+    {
+        UniTask<string> Serialize(object data);
+        UniTask<T?> Deserialize<T>(string json);
+    }
+
+    public class Serializer : ISerializer
+    {
+        public UniTask<string> Serialize(object data)
+        {
+            var task = UniTask.RunOnThreadPool(
+                () => JsonConvert.SerializeObject(data, Formatting.Indented)
+            );
+            return task;
+        }
+
+        public UniTask<T?> Deserialize<T>(string json)
+        {
+            var task = UniTask.RunOnThreadPool(
+                () => JsonConvert.DeserializeObject<T>(json)
+            );
+
+            return task;
+        }
+    }
+}
