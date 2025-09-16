f5c5de6d8c6a5dc69c7f54a943a1f4ffd4d3918b
noobnewbier <peter789b@gmail.com>
2025-09-16 20:58:33 +0100

    feat(core): added a way to handle debug flag but just realized I ain't making the thing that uses debug flag rn. Would be useful later though

---
+++ Assets/Code/NonebNi/Core/Debug/DebugFlagRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using NonebNi.Core.Save;
+using StrongInject;
+
+namespace NonebNi.Core.Debug
+{
+    public interface IDebugFlagRepository
+    {
+        bool Get(string flagName);
+        void Set(string flagName, bool value);
+        IEnumerable<(string flag, bool value)> GetAll();
+    }
+
+    public class DebugFlagRepository : IDebugFlagRepository, IRequiresAsyncInitialization
+    {
+        private const string SaveFilePath = "DebugFlags.json";
+        private readonly ISaveService _saveService;
+
+        private DebugFlags _debugFlags = new();
+
+        public DebugFlagRepository(ISaveService saveService)
+        {
+            _saveService = saveService;
+        }
+
+        public bool Get(string flagName) => _debugFlags.Flags.GetValueOrDefault(flagName, false);
+
+        public void Set(string flagName, bool value)
+        {
+            _debugFlags.Flags[flagName] = value;
+
+            // Forget(), and regret later.
+            _saveService.Save(_debugFlags, SaveFilePath).Forget();
+        }
+
+        public IEnumerable<(string flag, bool value)> GetAll()
+        {
+            foreach (var (key, value) in _debugFlags.Flags) yield return (key, value);
+        }
+
+        public async ValueTask InitializeAsync()
+        {
+            /*
+             * Note:
+             * At some point we might want a more well-defined post ctor initialization mechanism,
+             * or a way that ctor create this and it will be in a "valid" state right off the bat
+             * notably one that can handle dependency chain
+             * But now this is good enough for now.
+             */
+            var loadData = await _saveService.Load<DebugFlags>(SaveFilePath);
+            if (loadData == null) return;
+
+            _debugFlags = loadData;
+        }
+    }
+}
---
+++ Assets/Code/NonebNi/Core/Debug/DebugFlags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NonebNi.Core.Debug
+{
+    [Serializable]
+    public class DebugFlags
+    {
+        /*
+         * Note:
+         * if obsoleted keys become a real headache, we can always implement a time based obsolete mechanism,
+         * where if keys aren't accessed for X amount of time it's hidden from the debug view and eventually destroyed?
+         *
+         * Anyway, I can regret later.
+         */
+        public Dictionary<string, bool> Flags = new();
+    }
+}
