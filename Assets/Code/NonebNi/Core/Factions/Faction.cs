5cefa02196bce8ee523bfe88f97416eef08d75ef
noobnewbier <peter789b@gmail.com>
2025-09-16 20:57:51 +0100

    feat(core): faction now has colour, as debug tool needed this to visualize stuffs

--- Assets/Code/NonebNi/Core/Factions/Faction.cs
+++ Assets/Code/NonebNi/Core/Factions/Faction.cs
@@ -10,8 +10,10 @@ namespace NonebNi.Core.Factions
     public class Faction
     {
         [SerializeField] private string id;
-        [SerializeField] private bool isPlayerControlled; //TODO: feels weird do we actually need this
-        [SerializeField] private List<string> allies = new();
+        [SerializeField] private bool isPlayerControlled;                       //TODO: feels weird do we actually need this
+        [SerializeField] private List<string> allies = new();                   //todo: note multiple faction needs to have their allies in sync, this is quite error prone but might be a feature...?
+        [field: SerializeField] public Color FactionColor { get; private set; } //Note: AI debug tools uses this, but it feels wrong that this lives here?
+
 
         public Faction(string id, bool isPlayerControlled)
         {
