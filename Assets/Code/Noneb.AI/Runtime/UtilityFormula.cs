fd242c0844d71b2d2b723fa3772d611073cacd83
noobnewbier <peter789b@gmail.com>
2025-09-23 20:54:55 +0100

    refactor(ai): change how we construct UtilityFormula - we cannot rely on the graph editor for BehaviourGraph.

--- Assets/Code/Noneb.AI/Runtime/UtilityFormula.cs
+++ Assets/Code/Noneb.AI/Runtime/UtilityFormula.cs
@@ -1,50 +1,86 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Unity.Logging;
+using UnityEngine;
+using UnityUtils.Constants;
+using UnityUtils.Serialization;
 
 namespace Noneb.AI.Runtime
 {
-    public class UtilityFormula
+    /// <summary>
+    /// The sole reason that this is not in GraphToolkit is that I am too lazy.
+    /// This will NEVER run any ordinary AI behaviour. The purpose of this node is to let us use tree structure to define a
+    /// formula similar to the math dark art GDC talk.
+    /// In the ideal world I would write my own graph toolkit. But for now, this is good enough.
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(UtilityFormula), menuName = MenuName.Data + nameof(UtilityFormula))]
+    public class UtilityFormula : ScriptableObject
     {
-        private readonly Dictionary<UtilityFormula, float> _childAndWeight;
-        private readonly string _tag;
+        /*
+         * A graph editor is still nice as it allow me to give comment in the structure.
+         * For now though, this is good enough.
+         */
 
-        public UtilityFormula(string tag, Dictionary<UtilityFormula, float> childAndWeight)
+        //todo: handle negative 1 -> where the choice should never be taken. or just a way to flag invalid choice.
+        //Todo: something to flag that the tag isn't used by the code.
+        [SerializeField] private FormulaImp definitionRoot = new();
+
+        public float Calculate(Utility utility) => definitionRoot.Calculate(utility);
+
+
+        /// <summary>
+        /// Exists to help me reason the recursive logic, perhaps not strictly necessary.
+        /// </summary>
+        [Serializable]
+        private class FormulaImp
         {
-            _tag = tag;
-            _childAndWeight = childAndWeight;
-        }
+            [SerializeField] private string tag = string.Empty;
+            [SerializeField] private SerializableDictionary<FormulaImp, float> childAndWeight = new();
 
-        public float Calculate(Utility utility)
-        {
-            if (!_childAndWeight.Any())
-                //no children - this is the leaf, return it and let parent handle the weight.
-                return utility[_tag];
-
-            var scoreAndWeight = new List<(float score, float weight)>();
-            foreach (var (formula, weight) in _childAndWeight)
+            public float Calculate(Utility utility)
             {
-                if (weight == 0) continue;
+                if (!childAndWeight.Any())
+                    //no children - this is the leaf, return it and let parent handle the weight.
+                    return utility[tag];
 
-                var score = formula.Calculate(utility);
-                scoreAndWeight.Add((score, weight));
+                var scoreAndWeight = new List<(float score, float weight)>();
+
+                foreach (var (formula, weight) in childAndWeight)
+                {
+                    if (weight == 0) continue;
+
+                    var score = formula.Calculate(utility);
+                    scoreAndWeight.Add((score, weight));
+                }
+
+                var totalWeight = scoreAndWeight.Select(i => i.weight).Sum();
+                var toReturn = 0f;
+
+                foreach (var (score, weight) in scoreAndWeight)
+                {
+                    var weightedScore = score * (weight / totalWeight);
+                    toReturn += weightedScore;
+                }
+
+                if (utility[tag] > 0)
+                {
+                    Log.Error("This doesn't really make sense, the formula work by having the action giving context for the leaf node, and we reconstruct the weight up the tree. I don't know what is happening but I will try to play along here");
+                    toReturn += utility[tag];
+                }
+
+                return toReturn;
             }
 
-            var totalWeight = scoreAndWeight.Select(i => i.weight).Sum();
-            var toReturn = 0f;
-            foreach (var (score, weight) in scoreAndWeight)
+            public override string ToString()
             {
-                var weightedScore = score * (weight / totalWeight);
-                toReturn += weightedScore;
+                var label = string.IsNullOrWhiteSpace(tag) ?
+                    "EMPTY" :
+                    tag;
+                var child = childAndWeight.Select(pair => $"{pair.Key}_{pair.Value}");
+                var childString = string.Join("|", child);
+                return $"{label} - CHILD: {childString}";
             }
-
-            if (utility[_tag] > 0)
-            {
-                Log.Error("This doesn't really make sense, the formula work by having the action giving context for the leaf node, and we reconstruct the weight up the tree. I don't know what is happening but I will try to play along here");
-                toReturn += utility[_tag];
-            }
-
-            return toReturn;
         }
     }
 }
