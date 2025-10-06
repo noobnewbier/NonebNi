803fcea393dc0df61fc6f46ffed1e39f6d943c6b
noobnewbier <peter789b@gmail.com>
2025-10-06 22:21:52 +0100

    chore(core): making sure code gen from Unity's package happens with my core asm

---
+++ Assets/Code/NonebNi/Core/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+using Unity.Properties;
+
+// This assembly is the default dynamic assembly generated Castle DynamicProxy, 
+// used by Moq. Paste in a single line.   
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: GeneratePropertyBagsForAssembly]
