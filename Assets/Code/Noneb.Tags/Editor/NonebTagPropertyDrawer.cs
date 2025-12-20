7d50d71b28df991cb586cc7903328eefb9605b48
noobnewbier <peter789b@gmail.com>
2025-12-20 18:23:17 +0000

    fix(editor): fix a bug where two tag property drawer is sharing the same drawer

--- Assets/Code/Noneb.Tags/Editor/NonebTagPropertyDrawer.cs
+++ Assets/Code/Noneb.Tags/Editor/NonebTagPropertyDrawer.cs
@@ -1,4 +1,5 @@
-﻿using Noneb.Tags.Editor.TagPickers;
+﻿using System.Collections.Generic;
+using Noneb.Tags.Editor.TagPickers;
 using Noneb.Tags.Runtime;
 using UnityEditor;
 using UnityEngine;
@@ -8,14 +9,22 @@ namespace Noneb.Tags.Editor
     [CustomPropertyDrawer(typeof(NonebTag))]
     public class NonebTagPropertyDrawer : PropertyDrawer
     {
-        private readonly TagPicker _tagPicker = new ();
+        private static readonly Dictionary<string, TagPicker> PickersCache = new ();
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             using (new EditorGUI.PropertyScope(position, label, property))
             {
-                _tagPicker.Draw(position, label, property);
+                GetOrCreatePicker(property).Draw(position, label, property);
             }
+        }
+
+        private static TagPicker GetOrCreatePicker(SerializedProperty property)
+        {
+            // note: might be better to differentiate with serialized object as well...?
+            if (!PickersCache.TryGetValue(property.propertyPath, out var picker)) PickersCache[property.propertyPath] = picker = new ();
+
+            return picker;
         }
     }
 }
