5b5263e5e3eb4eb662542772b6a364ea81666c1a
noobnewbier <peter789b@gmail.com>
2025-12-23 22:47:28 +0000

    fix(core): fixing a bug where sequence is getting evaluated JIT whilst the animation is running, causing runtime bug

--- Assets/Code/NonebNi/Core/Effects/EffectResult.cs
+++ Assets/Code/NonebNi/Core/Effects/EffectResult.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NonebNi.Core.Actions;
 using NonebNi.Core.Entities;
@@ -10,15 +11,19 @@ namespace NonebNi.Core.Effects
     /// Combo is valid when either the target is in <see cref="ValidComboReceiver" />, or if the action is initiated by
     /// <see cref="ValidComboReceiver" />
     /// </summary>
-    public record EffectResult(IEnumerable<ISequence> Sequences, ISet<IActionTarget> ValidComboReceiver, ISet<EntityData> ValidComboCarrier)
+    public record EffectResult(IReadOnlyList<ISequence> Sequences, ISet<IActionTarget> ValidComboReceiver, ISet<EntityData> ValidComboCarrier)
     {
-        public EffectResult(IEnumerable<ISequence> Sequences) : this(Sequences, new HashSet<IActionTarget>(), new HashSet<EntityData>()) { }
-        public static EffectResult Empty { get; } = new(Enumerable.Empty<ISequence>());
+        public EffectResult(IEnumerable<ISequence> Sequences, ISet<IActionTarget> ValidComboReceiver, ISet<EntityData> ValidComboCarrier) : this(Sequences.ToArray(), ValidComboReceiver, ValidComboCarrier) { }
+        public EffectResult(IReadOnlyList<ISequence> Sequences) : this(Sequences, new HashSet<IActionTarget>(), new HashSet<EntityData>()) { }
+        public EffectResult(IEnumerable<ISequence> Sequences) : this(Sequences.ToArray()) { }
+
+        public static EffectResult Empty { get; } = new (ArraySegment<ISequence>.Empty);
         public bool CanCombo => ValidComboReceiver.Any() || ValidComboCarrier.Any();
 
         public EffectResult Concat(EffectResult result) =>
-            new(
-                Sequences.Concat(result.Sequences),
+            new
+            (
+                Sequences.Concat(result.Sequences).ToArray(),
                 ValidComboReceiver.Union(result.ValidComboReceiver).ToHashSet(),
                 ValidComboCarrier.Union(result.ValidComboCarrier).ToHashSet()
             );
