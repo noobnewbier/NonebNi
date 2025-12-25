6fa7c9274748956101a3b5c846d19b62c167a683
noobnewbier <peter789b@gmail.com>
2025-12-25 19:58:58 +0000

    feat(core): GetCoordinatesBetween can now work with hexes not on the same axis

--- Assets/Code/NonebNi/Core/Coordinates/Coordinate.cs
+++ Assets/Code/NonebNi/Core/Coordinates/Coordinate.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using Noneb.Logs.Runtime;
 using NonebNi.Core.Actions;
 using NonebNi.Core.Maps;
 using UnityEngine;
@@ -50,8 +49,8 @@ namespace NonebNi.Core.Coordinates
 
         public Coordinate Normalized()
         {
-            var normalizedCoordInVec = Vector3.Normalize(new Vector3(x, Y, z));
-            return new Coordinate(Mathf.RoundToInt(normalizedCoordInVec.x), Mathf.RoundToInt(normalizedCoordInVec.z));
+            var normalizedCoordInVec = Vector3.Normalize(new (x, Y, z));
+            return new (Mathf.RoundToInt(normalizedCoordInVec.x), Mathf.RoundToInt(normalizedCoordInVec.z));
         }
 
         public int DistanceTo(Coordinate coordinate)
@@ -97,42 +96,49 @@ namespace NonebNi.Core.Coordinates
         public bool IsOnSameAxisWith(Coordinate coordinate) =>
             X == coordinate.X || Y == coordinate.Y || Z == coordinate.Z;
 
+        public int MinDiffInAxis(Coordinate coordinate) =>
+            Mathf.Min
+            (
+                Mathf.Abs(X - coordinate.X),
+                Mathf.Abs(Y - coordinate.Y),
+                Mathf.Abs(Z - coordinate.Z)
+            );
+
+        /// <summary>
+        /// Given distance to coordinate, is there are way to move the same distance but zig zagging more.
+        /// 1 for max zigzaggy, 0 for stuffs that's on same axis.
+        /// </summary>
         public IEnumerable<Coordinate> GetCoordinatesBetween(Coordinate coordinate)
         {
-            if (!IsOnSameLineWith(coordinate))
+            //https://www.redblobgames.com/grids/hexagons/#line-drawing
+            var dist = DistanceTo(coordinate);
+            const float xNudge = 1e-6f;
+            const float zNudge = -3e-6f;
+            for (var i = 1; i < dist; i++)
             {
-                Log.Error("Map", "This method only support coordinates on a straight line at the moment!");
-                yield break;
+                var t = (float)i / dist;
+                var lerpX = Mathf.Lerp(X + xNudge, coordinate.X + xNudge, t);
+                var lerpZ = Mathf.Lerp(Z + zNudge, coordinate.Z + zNudge, t);
+                yield return Round(lerpX, lerpZ);
             }
 
-            if (Z == coordinate.Z)
-            {
-                var minX = Math.Min(X, coordinate.X);
-                var maxX = Math.Max(X, coordinate.X);
-                for (var xInBetween = minX + 1; xInBetween < maxX; xInBetween++)
-                    yield return new Coordinate(xInBetween, Z);
-            }
-            else if (Z == coordinate.Z)
-            {
-                var minZ = Math.Min(Z, coordinate.Z);
-                var maxZ = Math.Max(Z, coordinate.Z);
-                for (var zInBetween = minZ + 1; zInBetween < maxZ; zInBetween++)
-                    yield return new Coordinate(X, zInBetween);
-            }
-            else if (Y == coordinate.Y)
-            {
-                var minZ = Math.Min(Z, coordinate.Z);
-                var maxZ = Math.Max(Z, coordinate.Z);
-                var maxX = Math.Max(X, coordinate.X);
-                for (var i = 1; i < maxZ - minZ; i++)
-                {
-                    var xInBetween = maxX - i;
-                    var zInBetween = minZ + i;
+            yield break;
 
-                    yield return new Coordinate(xInBetween, zInBetween);
-                }
+
+            Coordinate Round(float floatX, float floatZ)
+            {
+                var roundedX = Mathf.RoundToInt(floatX);
+                var roundedZ = Mathf.RoundToInt(floatZ);
+
+                var diffX = floatX - roundedX;
+                var diffZ = floatZ - roundedZ;
+
+                return diffX >= diffZ ?
+                    new (roundedX + Mathf.RoundToInt(diffX + diffZ * 0.5f), roundedZ) :
+                    new (roundedX, roundedZ + Mathf.RoundToInt(diffZ + diffX * 0.5f));
             }
         }
+
 
         public override int GetHashCode()
         {
---
+++ Assets/Code/NonebNi/Develop/Ui/LineHexesTestScript.cs
@@ -0,0 +1,91 @@
+﻿using NonebNi.Core.Coordinates;
+using NonebNi.Core.Maps;
+using NonebNi.Terrain;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityUtils.Editor;
+using static NonebNi.Develop.TestScriptHelpers;
+
+namespace NonebNi.Develop
+{
+    public class LineHexesTestScript : MonoBehaviour
+    {
+        private readonly TerrainConfigData _configData = TerrainConfigData.Default;
+
+        private bool _isInitialised;
+        private IReadOnlyMap _map = null!;
+
+        private Coordinate _pointA = Coordinate.Zero;
+        private Coordinate _pointB = Coordinate.Zero;
+        private CoordinateAndPositionService _service = null!;
+
+        private void Awake()
+        {
+            _isInitialised = true;
+
+            _service = new (_configData);
+            _map = new Map(10, 10);
+        }
+
+        private void Update()
+        {
+            ProcessInput();
+        }
+
+        private void OnGUI()
+        {
+            var startingRect = new Rect(10, 10, 150, 25);
+            var rect = startingRect;
+            if (!_isInitialised)
+            {
+                GUI.Label(rect, "Start the scene to test");
+                return;
+            }
+
+            GUI.Label(rect, "Left Click for Point A");
+            rect.y += 25;
+
+            GUI.Label(rect, "Right Click for Point B");
+            rect.y += 25;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!_isInitialised) return;
+
+            DrawGridUsingGizmos(_service, _map);
+
+            var aPos = _service.FindPosition(_pointA);
+            var bPos = _service.FindPosition(_pointB);
+
+            using (new NonebEditorGUI.GizmosColorScope(Color.green))
+            {
+                Gizmos.DrawSphere(aPos, 0.25f);
+                Gizmos.DrawSphere(bPos, 0.25f);
+            }
+
+            using (new NonebEditorGUI.GizmosColorScope(Color.yellow))
+            {
+                Gizmos.DrawLine(aPos, bPos);
+            }
+
+            using (new NonebEditorGUI.GizmosColorScope(Color.red))
+            {
+                foreach (var coord in _pointA.GetCoordinatesBetween(_pointB))
+                {
+                    var inBetween = _service.FindPosition(coord);
+                    Gizmos.DrawSphere(inBetween, 0.5f);
+                }
+            }
+        }
+
+        private void ProcessInput()
+        {
+            var (success, pos) = FindMousePosInWorld(_configData.GridPlane);
+            if (!success) return;
+
+            if (Mouse.current.leftButton.wasReleasedThisFrame) _pointA = _service.NearestCoordinateForPoint(pos);
+            if (Mouse.current.rightButton.wasReleasedThisFrame) _pointB = _service.NearestCoordinateForPoint(pos);
+        }
+    }
+}
