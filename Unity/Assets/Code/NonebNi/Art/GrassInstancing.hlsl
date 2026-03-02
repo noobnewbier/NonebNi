d4bada3fb38643f5196575bec5c87b3c8806f854
noobnewbier <peter789b@gmail.com>
2026-03-02 20:51:21 +0000

    feat(art): grass can now be shaded, yay

--- Unity/Assets/Code/NonebNi/Art/GrassInstancing.hlsl
+++ Unity/Assets/Code/NonebNi/Art/GrassInstancing.hlsl
@@ -4,7 +4,34 @@
 #include "InstancingData.hlsl"
 #include "GPUInstancingSetUpForShaderGraph.hlsl"
 
-void Instancing_float(float3 pos, float instanceId, out float3 outPos, out float3 outNormal)
+#ifndef SHADERGRAPH_PREVIEW
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/RealtimeLights.hlsl"
+#endif
+
+
+/*
+ * Note:
+ * most likely necessary for shadows stuffs
+ * https://github.com/Cyanilux/URP_ShaderGraphCustomLighting/blob/19fac6a6126a9c25d8f226b38bb12c7360557ff6/CustomLighting.hlsl#L12
+ */
+#ifndef SHADERGRAPH_PREVIEW
+#if SHADERPASS != SHADERPASS_FORWARD && SHADERPASS != SHADERPASS_GBUFFER
+// #if to avoid "duplicate keyword" warnings if this is included in a Lit Graph
+
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS _MAIN_LIGHT_SHADOWS_CASCADE _MAIN_LIGHT_SHADOWS_SCREEN
+#pragma multi_compile_fragment _ _SHADOWS_SOFT _SHADOWS_SOFT_LOW _SHADOWS_SOFT_MEDIUM _SHADOWS_SOFT_HIGH
+#pragma multi_compile _ _ADDITIONAL_LIGHTS_VERTEX _ADDITIONAL_LIGHTS
+#pragma multi_compile_fragment _ _ADDITIONAL_LIGHT_SHADOWS
+#pragma multi_compile _ _CLUSTER_LIGHT_LOOP
+
+// Left some keywords (e.g. light layers, cookies) in subgraphs to help avoid unnecessary shader variants
+// But means if those subgraphs are nested in another, you'll need to copy the keywords from blackboard
+
+#endif
+#endif
+
+
+void Instancing_float(float3 pos, float instanceId, out float3 outPos, out float3 outNormal, out float3 outInstanceRootPos)
 {
     InstanceData data = _PerInstanceData[instanceId];
     VertInstancingSetup(data);
@@ -12,6 +39,31 @@ void Instancing_float(float3 pos, float instanceId, out float3 outPos, out float
     outPos = pos;
     //todo: normal feels wrong
     outNormal = mul(data.trsMatrix, float4(0., 1., 0., 0.)).xyz;
+    outInstanceRootPos = data.trsMatrix._14_24_34;
+}
+
+
+void ShadowColor_float(float3 baseColor, float3 normalWs, float3 positionWs, out float3 finalColor)
+{
+    float3 lightDirection = normalize(float3(1, 1, -0.4));
+    float3 lightColor = float3(1, 1, 1);
+    float lightAttenuation = 1;
+
+    #ifndef SHADERGRAPH_PREVIEW
+
+    float4 shadowCoord = TransformWorldToShadowCoord(positionWs);
+    half4 shadowMask = half4(1, 1, 1, 1);
+    Light mainLight = GetMainLight(shadowCoord, positionWs, shadowMask);
+    lightAttenuation = mainLight.distanceAttenuation * mainLight.shadowAttenuation;
+    lightColor = mainLight.color;
+    lightDirection = mainLight.direction;
+    #endif
+
+    float nDotL = max(0.0, dot(normalWs, lightDirection));
+    float halfLambert = pow(nDotL * 0.5 + 0.5, 2.);
+    float3 halfLambertDiffuse = halfLambert * baseColor;
+
+    finalColor = halfLambertDiffuse * lightAttenuation * lightColor;
 }
 
 #endif
