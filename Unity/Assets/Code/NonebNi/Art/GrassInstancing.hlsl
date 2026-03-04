fd710267732a3dfec2727dd54c1d63f754f176dd
noobnewbier <peter789b@gmail.com>
2026-03-04 21:24:31 +0000

    feat(art): grass shadow is now heavily biased on the base color, so it at least looks green even when it's under shadow

--- Unity/Assets/Code/NonebNi/Art/GrassInstancing.hlsl
+++ Unity/Assets/Code/NonebNi/Art/GrassInstancing.hlsl
@@ -30,6 +30,13 @@
 #endif
 #endif
 
+float inverseLerp(float from, float to, float value)
+{
+    float interpolate = (value - from) / (to - from);
+    interpolate = saturate(interpolate);
+
+    return interpolate;
+}
 
 void Instancing_float(float3 pos, float instanceId, out float3 outPos, out float3 outNormal, out float3 outInstanceRootPos)
 {
@@ -43,7 +50,15 @@ void Instancing_float(float3 pos, float instanceId, out float3 outPos, out float
 }
 
 
-void ShadowColor_float(float3 baseColor, float3 normalWs, float3 positionWs, out float3 finalColor)
+void ShadowColor_float(float3 baseColor,
+                       float featherRange,
+                       float3 firstShadeColor,
+                       float firstShadeStep,
+                       float3 secondShadeColor,
+                       float secondShadeStep,
+                       float3 normalWs,
+                       float3 positionWs,
+                       out float3 finalColor)
 {
     float3 lightDirection = normalize(float3(1, 1, -0.4));
     float3 lightColor = float3(1, 1, 1);
@@ -57,13 +72,22 @@ void ShadowColor_float(float3 baseColor, float3 normalWs, float3 positionWs, out
     lightAttenuation = mainLight.distanceAttenuation * mainLight.shadowAttenuation;
     lightColor = mainLight.color;
     lightDirection = mainLight.direction;
+
     #endif
 
-    float nDotL = max(0.0, dot(normalWs, lightDirection));
-    float halfLambert = pow(nDotL * 0.5 + 0.5, 2.);
-    float3 halfLambertDiffuse = halfLambert * baseColor;
+    float shadowStrength = 1. - lightAttenuation;
+    float shadeRange = inverseLerp(firstShadeStep, secondShadeStep, shadowStrength);
+    float isSecondShade = shadeRange >= 1;
+    float3 shadeColor = lerp(firstShadeColor, secondShadeColor, isSecondShade);
+    float isShaded = step(firstShadeStep, shadowStrength);
 
-    finalColor = halfLambertDiffuse * lightAttenuation * lightColor;
+    float3 diffuse = lerp(baseColor, shadeColor, isShaded);
+    
+    float nDotL = saturate(dot(normalWs, lightDirection));
+    float halfLambert = pow(nDotL * 0.5 + 0.5, 2.);
+    float3 halfLambertDiffuse =  diffuse * (lightColor * halfLambert);
+
+    finalColor = halfLambertDiffuse;
 }
 
 #endif
