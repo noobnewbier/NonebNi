40fe4d96c0210d224a2abaa1248e6121bd90f578
noobnewbier <peter789b@gmail.com>
2026-05-11 21:43:54 +0100

    feat(art): grass swaying is now break up into different time step so they are not rotating at the same time all the time

---
+++ Unity/Assets/Code/NonebNi/Art/Utils/ShaderGraphUtils.hlsl
@@ -0,0 +1,12 @@
+﻿#ifndef NONEB_SHADER_GRAPH_UTILS
+#define NONEB_SHADER_GRAPH_UTILS  
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/ShaderVariablesFunctions.hlsl"
+#include "UnityInstancing.cginc"
+
+void RoundToNearestMultiple_float(float input, float n, out float output)
+{
+    output = floor(input / n) * n;
+}
+
+#endif
